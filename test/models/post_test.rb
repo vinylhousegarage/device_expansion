f5d164ebde078f7f6e04be8c9408078d7344f580
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-01 09:08:51 +0900

    Add test/models/post_test.rb for Resolving foreign key constraint violations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,11 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "creates post with valid user_id" do
+    user = User.find_by(name: "ゲスト１")
+    assert_not_nil user, "User with name 'ゲスト１' not found"
+    post = Post.create!(name: "Test Post", user_id: user.id)
+
+    assert post.valid?
+  end
 end
