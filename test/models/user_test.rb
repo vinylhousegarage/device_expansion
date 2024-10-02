1af1610693130339aebeaeea9a1c5a13546ac1e1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-02 10:56:43 +0900

    Delete 'creates post with valid user_id' test from test/user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,13 +4,4 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
-  USERS = %w[ゲスト１ ゲスト２ ゲスト３ ゲスト４ ゲスト５ 集計担当].freeze
-
-  def test_users_exist
-    USERS.each do |user_name|
-      user = User.find_by(name: user_name)
-      assert_not_nil user, "#{user_name} should exist in the users table"
-    end
-  end
 end
