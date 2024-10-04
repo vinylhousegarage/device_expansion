6c56e0e6ac956c52777ac34c81cf870e4afc9ba6
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-04 09:59:09 +0900

    Delete Rails.application.load_seed to setup method in post_test.rb

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -7,8 +7,7 @@ class PostTest < ActiveSupport::TestCase
 
   # 初期データを挿入しユーザーを取得
   setup do
-    Rails.application.load_seed
-    @user = User.find_by(name: "ゲスト１")
+    @user = User.find_or_create_by(name: "ゲスト１")
   end
 
   # name属性のバリデーションをテスト
