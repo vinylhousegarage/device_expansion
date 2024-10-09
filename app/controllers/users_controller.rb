d5bbdb12052630815cd09e0d1eaf06f1b5b8b373
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-09 13:36:24 +0900

    Add login action in users_controller.rb (CI)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+  # 仮に、管理者ユーザーをフィクスチャまたはデータベースから取得
+  def login
+    admin_user = User.find_by(name: "集計担当")
+    session[:user_id] = admin_user.id
+    redirect_to users_path
+  end
 end
