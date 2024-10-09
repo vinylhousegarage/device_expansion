c5c29eabead89aa31c44795c48825c3c34d077cb
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-09 22:55:16 +0900

    Update GUEST_USERS to include both ID and name for guest users (CI)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
 
-  # 投稿者を定義
-  GUEST_NAMES = ['ゲスト１', 'ゲスト２', 'ゲスト３', 'ゲスト４', 'ゲスト５'].freeze
+  # 投稿者をIDと名前で定義
+  GUEST_USERS = {
+    1 => 'ゲスト１',
+    2 => 'ゲスト２',
+    3 => 'ゲスト３',
+    4 => 'ゲスト４',
+    5 => 'ゲスト５'
+  }.freeze
 end
