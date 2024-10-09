b8ecc0da5c746103b6d82470dd9a53a58e60c9f1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-09 20:25:56 +0900

    Add test for new user view

---
+++ test/views/users_new_view_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersNewViewTest < ActionView::TestCase
+  fixtures :users
+
+  test "renders the new user invitation form" do
+    assign(:users, [users(:guest_1), users(:guest_2)])
+    render template: 'users/new'
+    assert_select 'h2', 'みんなで香典集計'
+    assert_select "form[action=?][method=?]", login_form_user_path(users(:guest_1)), "post" do
+      assert_select "input[type=submit][value=?]", "ゲスト1さんを招待する"
+    end
+    assert_select "form[action=?][method=?]", login_form_user_path(users(:guest_2)), "post" do
+      assert_select "input[type=submit][value=?]", "ゲスト2さんを招待する"
+    end
+  end
+end
