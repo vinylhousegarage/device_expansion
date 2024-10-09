c6c9a41b3ae9f632c0c239aa57ea88f6460cc01d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-09 20:42:26 +0900

    Fix assign method issue in new user view test (CI)

--- test/views/users_new_view_test.rb
+++ test/views/users_new_view_test.rb
@@ -4,7 +4,7 @@ class UsersNewViewTest < ActionView::TestCase
   fixtures :users
 
   test "renders the new user invitation form" do
-    assign(:users, [users(:guest_1), users(:guest_2)])
+    @users = [users(:guest_1), users(:guest_2)]
     render template: 'users/new'
     assert_select 'h2', 'みんなで香典集計'
     assert_select "form[action=?][method=?]", login_form_user_path(users(:guest_1)), "post" do
