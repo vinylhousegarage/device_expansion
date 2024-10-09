f092c8a01702e3bdb79282533098afbc2e357381
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-09 12:55:32 +0900

    Add login action path in routes.rb (CI)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users do
+    collection do
+      post 'login'
+    end
+  end
 end
