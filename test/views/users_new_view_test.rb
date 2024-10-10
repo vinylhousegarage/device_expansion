389d7ac5dae009a750f13c0a7ef426b34d33394d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-10 12:38:29 +0900

    Rename guest users to poster users for clarity in fixtures/post.yml (CI)

--- test/views/users_new_view_test.rb
+++ test/views/users_new_view_test.rb
@@ -4,13 +4,13 @@ class UsersNewViewTest < ActionView::TestCase
   fixtures :users
 
   test "renders the new user invitation form" do
-    @users = [users(:guest_1), users(:guest_2)]
+    @users = [users(:poster_1), users(:poster_2)]
     render template: 'users/new'
     assert_select 'h2', 'みんなで香典集計'
-    assert_select "form[action=?][method=?]", login_form_user_path(users(:guest_1)), "post" do
+    assert_select "form[action=?][method=?]", login_form_user_path(users(:poster_1)), "post" do
       assert_select "button", "ゲスト１さんを招待する"
     end
-    assert_select "form[action=?][method=?]", login_form_user_path(users(:guest_2)), "post" do
+    assert_select "form[action=?][method=?]", login_form_user_path(users(:poster_2)), "post" do
       assert_select "button", "ゲスト２さんを招待する"
     end
   end
