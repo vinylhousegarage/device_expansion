dd2ca696d7d1194480bacf8366039f79b66ee272
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-10 15:36:36 +0900

    Refactor admin_users scope to use where for multiple admins (CI)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # 管理者を「集計担当」として定義
   ADMIN_USERS = ['集計担当'].freeze
   # 管理者をデータベースから取得するスコープを定義
-  scope :admin_users, -> { find_by(name: ADMIN_USERS) }
+  scope :admin_users, -> { where(name: ADMIN_USERS) }
   # 投稿者を定義
   POSTER_USERS = ['投稿者１', '投稿者２', '投稿者３', '投稿者４', '投稿者５'].freeze
   # 投稿者をデータベースから取得するスコープを定義
