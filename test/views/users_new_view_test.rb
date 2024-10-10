45f5d790a6a08b02e80df58569f95b49ec12c29a
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-10 13:25:00 +0900

    Rename guest users to poster users for clarity in UsersNewViewTest (CI)

--- test/views/users_new_view_test.rb
+++ test/views/users_new_view_test.rb
@@ -8,10 +8,10 @@ class UsersNewViewTest < ActionView::TestCase
     render template: 'users/new'
     assert_select 'h2', 'みんなで香典集計'
     assert_select "form[action=?][method=?]", login_form_user_path(users(:poster_1)), "post" do
-      assert_select "button", "ゲスト１さんを招待する"
+      assert_select "button", "投稿者１さんを招待する"
     end
     assert_select "form[action=?][method=?]", login_form_user_path(users(:poster_2)), "post" do
-      assert_select "button", "ゲスト２さんを招待する"
+      assert_select "button", "投稿者２さんを招待する"
     end
   end
 end
