ca26c3ea7f49c45c0b45129362c8c26d08aae275
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-10 13:08:13 +0900

    Add poster_users scope in User class (CI)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
   # 投稿者を定義
   POSTER_USERS = ['投稿者１', '投稿者２', '投稿者３', '投稿者４', '投稿者５'].freeze
+  # 投稿者をデータベースから取得するスコープを定義
+  scope :poster_users, -> { where(name: POSTER_USERS) }
 end
