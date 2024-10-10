0876323767940b47cf93281a0a85721ab6736757
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-10 14:38:48 +0900

    Refactor login action to use admin_user scope in UsersController (CI)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   # 仮に、管理者ユーザーをフィクスチャまたはデータベースから取得
   def login
-    admin_user = User.find_by(name: "集計担当")
+    @user = User.admin_user
     session[:user_id] = admin_user.id
     redirect_to users_path
   end
