9e254204ccbfc8c1e8b4095cd202e473c22e3370
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-10 12:17:24 +0900

    Refactor seeds.rb to separate poster and admin users for clarity

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-USERS = %w[ゲスト１ ゲスト２ ゲスト３ ゲスト４ ゲスト５ 集計担当].freeze
+# 投稿者を定義
+POSTER_USERS = %w[投稿者１ 投稿者２ 投稿者３ 投稿者４ 投稿者５].freeze
+# 管理者を「集計担当」名で定義
+ADMIN_USERS = %w[集計担当].freeze
+# 投稿者と管理者を統合
+USERS = (POSTER_USERS + ADMIN_USERS).freeze
 
+# find_or_create_byを使用してユーザーを作成
 USERS.each do |user_name|
   User.find_or_create_by(name: user_name)
 end
