07db596bece41136e4d16f99bf64ee3278a26d23
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-16 18:15:59 +0900

    Add detailed logging to deployment script for enhanced debugging

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Running bundle install..."
 bundle install
+
+echo "Running yarn install..."
 yarn install
+
+echo "Adding esbuild..."
 yarn add esbuild
+
+echo "Building assets..."
 yarn build
+
+echo "Precompiling assets..."
 bundle exec rails assets:precompile
+
+echo "Running database migrations..."
 bundle exec rails db:migrate
+
+echo "Seeding the database..."
 bundle exec rails db:seed
