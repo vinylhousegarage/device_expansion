7c594b76e85e8cd117d67acff622f47d27b70595
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-17 19:51:07 +0900

    Remove importmap-rails and related files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "./controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+import HelloController from "./controllers/hello_controller"
+
+const application = Application.start()
+application.register("hello", HelloController)
