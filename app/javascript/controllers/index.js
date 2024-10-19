3cf63f589e4c27c11c7c64029a01cb173cbc1f23
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-19 18:34:27 +0900

    chore: install stimulus to restore JavaScript module management

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,4 @@
-// Import and register all your controllers from the controllers directory
-import { Application } from "@hotwired/stimulus";
-
-// Initialize Stimulus Application
-const application = Application.start();
-
-// コントローラを自動で読み込む
-const context = require.context("./controllers", true, /\.js$/);
-application.load(context);
+// Import and register all your controllers from the importmap via controllers/**/*_controller
+import { application } from "controllers/application"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
