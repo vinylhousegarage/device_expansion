0289b1d4cca533b0aaddfea03d6bd39f0b52fcb2
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-20 16:40:46 +0900

    Include SECRET_KEY_BASE and importmap installation in render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,11 @@ set -o errexit
 
 export NODE_ENV=production
 
+export SECRET_KEY_BASE=${SECRET_KEY_BASE}
+
+echo "Installing importmap..."
+bin/rails importmap:install
+
 echo "Building assets..."
 yarn build:production
 
