9265669bf1f7f98b620948e74f46c56597d9f2d9
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-21 12:43:17 +0900

    Update application.js for esbuild with Turbo and Stimulus integration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import { Turbo } from "@hotwired/turbo-rails";
+Turbo.start();
+
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-loading";
+
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
