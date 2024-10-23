949dca5c8872af295f66b6719c72b0390a7ce9ad
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-23 09:08:35 +0900

    Enable digest for assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
   config.assets.precompile += %w( application.js application.css )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
