16809d9c37f0dc86f02876381b6a928ce6e53499
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-26 08:48:49 +0900

    Remove @hotwired/stimulus-loading and use @hotwired/stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@ import { Turbo } from "@hotwired/turbo-rails";
 Turbo.start();
 
 import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "@hotwired/stimulus-loading";
+import { definitionsFromContext } from "@hotwired/stimulus";
 
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
