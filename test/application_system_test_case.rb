404af50e81b053325029edb01663ebea303f4023
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 08:37:33 +0900

    Add host and timeot configuration to application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,4 +4,9 @@ require 'capybara/minitest'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { headless: true }
+
+  Capybara.server_host = '0.0.0.0'
+  Capybara.server_port = 3000
+
+  Capybara.default_max_wait_time = 5
 end
