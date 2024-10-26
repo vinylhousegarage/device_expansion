7b90ffe48748fc08476043b6c1a1e8a43cc0b36f
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 08:16:29 +0900

    Set up capybara test in application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
+require 'capybara/rails'
+require 'capybara/minitest'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { headless: true }
 end
