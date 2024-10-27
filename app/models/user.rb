9da5bbfd9d2688e0a3ddf2f9c356fa463c68d932
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 15:59:31 +0900

    Use %w literal for word array in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # 管理者をデータベースから取得するスコープを定義
   scope :admin_users, -> { where(name: ADMIN_USERS) }
   # 投稿者を定義
-  POSTER_USERS = ['投稿者１', '投稿者２', '投稿者３', '投稿者４', '投稿者５'].freeze
+  POSTER_USERS = %w[投稿者１ 投稿者２ 投稿者３ 投稿者４ 投稿者５].freeze
   # 投稿者をデータベースから取得するスコープを定義
   scope :poster_users, -> { where(name: POSTER_USERS) }
 
