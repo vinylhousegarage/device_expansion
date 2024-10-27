09f00ebbec2cd9e0c861cae2a49e38243e53fc23
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 14:12:34 +0900

    Use single quotes for Rubocop compliance in posts and users_controller_test.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
@@ -6,7 +6,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # end
 
   # 管理者の"集計担当"ユーザーでログインするテスト
-  test "should log in admin user and redirect to users path" do
+  test 'should log in admin user and redirect to users path' do
     admin_user = users(:admin)
     post login_users_path
     assert_equal admin_user.id, session[:user_id]
@@ -14,13 +14,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   # newアクション実行時のセッションID の空チェック
-  test "should reset session in new" do
+  test j'should reset session in new' do
     get new_user_path
     assert_nil session[:user_id]
   end
 
   # ページの表示をテスト
-  test "should get new user page" do
+  test 'should get new user page' do
     get new_user_path
     assert_response :success
   end
@@ -29,7 +29,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   fixtures :users
 
   # スコープ poster_users の参照をテスト
-  test "should display poster users in new action" do
+  test 'should display poster users in new action' do
     get new_user_path
     assert_select 'form[action=?]', login_form_user_path(users(:poster_1)), text: '投稿者１さんを招待する'
     assert_select 'form[action=?]', login_form_user_path(users(:poster_2)), text: '投稿者２さんを招待する'
