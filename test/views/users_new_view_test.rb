779f006ce2d14e3bcd7cd0cbe641a789dfad6987
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 14:58:32 +0900

    Update view test to expect h3 instead of h2

--- test/views/users_new_view_test.rb
+++ test/views/users_new_view_test.rb
@@ -6,7 +6,7 @@ class UsersNewViewTest < ActionView::TestCase
   test "renders the new user invitation form" do
     @users = [users(:poster_one), users(:poster_two)]
     render template: 'users/new'
-    assert_select 'h2', 'みんなで香典集計'
+    assert_select 'h3', 'みんなで香典集計'
     assert_select 'form[action=?][method=?]', login_form_user_path(users(:poster_one)), 'post' do
       assert_select 'button', '投稿者１さんを招待する'
     end
