9119c20d1c2a0776d1812894eec3099deeeb5c2e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 14:52:51 +0900

    Replace CRLF with LF and refactor symbol names, use single quotes for Rubocop

--- test/views/users_new_view_test.rb
+++ test/views/users_new_view_test.rb
@@ -4,14 +4,14 @@ class UsersNewViewTest < ActionView::TestCase
   fixtures :users
 
   test "renders the new user invitation form" do
-    @users = [users(:poster_1), users(:poster_2)]
+    @users = [users(:poster_one), users(:poster_two)]
     render template: 'users/new'
-    assert_select 'h3', 'みんなで香典集計'
-    assert_select "form[action=?][method=?]", login_form_user_path(users(:poster_1)), "post" do
-      assert_select "button", "投稿者１さんを招待する"
+    assert_select 'h2', 'みんなで香典集計'
+    assert_select 'form[action=?][method=?]', login_form_user_path(users(:poster_one)), 'post' do
+      assert_select 'button', '投稿者１さんを招待する'
     end
-    assert_select "form[action=?][method=?]", login_form_user_path(users(:poster_2)), "post" do
-      assert_select "button", "投稿者２さんを招待する"
+    assert_select 'form[action=?][method=?]', login_form_user_path(users(:poster_two)), 'post' do
+      assert_select 'button', '投稿者２さんを招待する'
     end
   end
 end
