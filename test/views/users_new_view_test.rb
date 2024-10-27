020c5305ea9dc7ff7cc22a831d2a9e18f669ed98
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-27 15:31:17 +0900

    Refactor symbol names in test/views/users_new_view_test.rb

--- test/views/users_new_view_test.rb
+++ test/views/users_new_view_test.rb
@@ -4,13 +4,13 @@ class UsersNewViewTest < ActionView::TestCase
   fixtures :users
 
   test "renders the new user invitation form" do
-    @users = [users(:poster_one), users(:poster_two)]
+    @users = [users(:first_poster), users(:second_poster)]
     render template: 'users/new'
     assert_select 'h3', 'みんなで香典集計'
-    assert_select 'form[action=?][method=?]', login_form_user_path(users(:poster_one)), 'post' do
+    assert_select 'form[action=?][method=?]', login_form_user_path(users(:first_poster)), 'post' do
       assert_select 'button', '投稿者１さんを招待する'
     end
-    assert_select 'form[action=?][method=?]', login_form_user_path(users(:poster_two)), 'post' do
+    assert_select 'form[action=?][method=?]', login_form_user_path(users(:second_poster)), 'post' do
       assert_select 'button', '投稿者２さんを招待する'
     end
   end
