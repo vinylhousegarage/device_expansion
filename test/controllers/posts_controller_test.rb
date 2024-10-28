5bf2570c0f198f5c97fda078b43b60ed6f91cf91
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-28 13:13:21 +0900

    Set posts new action test in post_controller_test.rb

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
+  # test 'the truth' do
   #   assert true
   # end
+
+  # セッションデータを設定
+  setup do
+    @user = users(:first_poster)
+    @post = posts(:first_post)
+    @session_data = { user_id: @user.id }
+  end
+
+  # newアクションをテスト
+  test 'should get new for existing user' do
+    get new_post_path, params: {}, session: @session_data
+
+    assert_response :success
+    assert_select 'form[action=?]', posts_path
+    assert_select 'div.post', text: @post.title
+  end
 end
