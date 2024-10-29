ab0e02a8a0c6c53725773a9e1542a93ebe010dd7
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-29 17:24:35 +0900

    Add new action log output method to PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   def new
+    logger.debug "Session user_id: #{session[:user_id]}"
+    logger.debug "Request params: #{params.inspect}"
+
     @user = User.find_from_session(session)
     @post = Post.new
     @posts = Post.by_user(@user.id)
