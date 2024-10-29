07210533ccaf7456202f8638ad9bda5c60aab946
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-29 21:13:21 +0900

    Refactor: Pass user ID via params to login_poster_users_path in test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,11 +5,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  # 投稿者を設定
+  setup do
+    @user = users(:first_poster)
+    @admin_user = users(:admin)
+  end
+
   # 管理者の"集計担当"ユーザーでログインするテスト
   test 'should log in admin user and redirect to users path' do
-    admin_user = users(:admin)
     post login_users_path
-    assert_equal admin_user.id, session[:user_id]
+    assert_equal @admin_user.id, session[:user_id]
     assert_redirected_to users_path
   end
 
@@ -36,11 +41,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_select 'form[action=?]', login_form_user_path(users(:admin)), count: 0
   end
 
-  # 投稿者を設定
-  setup do
-    @user = users(:first_poster)
-  end
-
   # QRコードの生成をテスト
   test 'should get login form and generate QR code' do
     svg_expected_count = 1
@@ -48,10 +48,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'svg', svg_expected_count
   end
-end
 
-# users#login_poster のパスをテスト
-test 'should get new user page' do
-  post login_poster_user_path, params: { id: @user.id }
-  assert_response :success
+  # 投稿者の"投稿者１"ユーザーでログインするテスト
+  test 'should log in first_poster user and redirect to new_post path' do
+    post login_poster_users_path, params: { id: @user.id }
+    assert_equal @user.id, session[:user_id]
+    assert_redirected_to new_post_path
+  end
 end
