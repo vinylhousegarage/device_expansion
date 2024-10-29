33d215d6bdc309effa4fec5748a5a6b5ef90abc3
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-29 10:16:18 +0900

    Store user_id in session as an integer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def new
-    session[:user_id] = params[:id]
+    session[:user_id] = params[:id].to_i
     @user = User.find_from_session(session)
     @post = Post.new
     @posts = Post.by_user(@user.id)
