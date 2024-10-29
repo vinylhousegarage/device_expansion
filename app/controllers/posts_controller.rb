c0f9292ae820fc74bac99af35d6ee021e102cfbf
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-29 10:08:24 +0900

    Fix missing user_id in session for QR code generation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def new
+    session[:user_id] = params[:id]
     @user = User.find_from_session(session)
     @post = Post.new
     @posts = Post.by_user(@user.id)
