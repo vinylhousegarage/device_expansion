7c47b4b85b14d1d8adc862235148cc461c4cd26c
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-29 14:13:26 +0900

    Fix lintcheck error in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   # QRコードを取得し@svgに格納
   def login_form
     @user = User.find(params[:id])
-    @svg = @user.generate_qr_code_session(session)
+    @svg = @user.generate_qr_code(session)
   end
 end
