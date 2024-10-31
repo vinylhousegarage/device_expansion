1604a378f70962352426ab082fb6849b68d21adb
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-31 20:56:28 +0900

    Add lodder.debug to new action in PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def new
+    logger.debug "Session user_id: #{session[:user_id]}"
     @user = User.find_by(id: session[:user_id])
     @post = Post.new
     @posts = Post.by_user(@user.id)
