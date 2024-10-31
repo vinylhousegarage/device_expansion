3ab5396c0dba11dd46fe4a78060826f2bfa78a03
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-31 21:32:25 +0900

    Add redirect follow to log_in_as helper in posts_controller_test.rb

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -13,7 +13,8 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
   # ログインヘルパーメソッドを定義
   def log_in_as(user)
-    post login_form_user_path(user.id), params: { session: { user_id: user.id } }
+    post login_poster_user_path(user.id), params: { id: user.id }
+    follow_redirect!
   end
 
   # newアクションをテスト
