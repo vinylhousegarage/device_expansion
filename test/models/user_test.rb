8b4f2f604f4618edb9507c6e0612aa5ccf227002
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-31 12:02:38 +0900

    Change new_post path test to login_poster in UserTest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,6 +39,23 @@ class UserTest < ActiveSupport::TestCase
     assert_equal '集計担当', admin_user.name
   end
 
+  # QRコードがSVG形式で生成されていることを確認するテスト
+  test 'QR code is generated in SVG format' do
+    svg = @user.generate_qr_code_for_login_poster
+
+    assert_includes svg, '<svg'
+  end
+
+  private
+
+  # login_poster のURLを取得
+  def login_poster_url(user)
+    Rails.application.routes.url_helpers.login_poster_user_url(
+      user,
+      host: 'https://device-expansion.onrender.com'
+    )
+  end
+
   # User.find_from_sessionメソッドのテスト
   test 'should find user from session' do
     user = users(:first_poster)
@@ -61,20 +78,3 @@ class UserTest < ActiveSupport::TestCase
     assert_nil User.find_from_session(session)
   end
 end
-
-  # QRコードがSVG形式で生成されていることを確認するテスト
-  test 'QR code is generated in SVG format' do
-    svg = @user.generate_qr_code
-
-    assert_includes svg, '<svg'
-  end
-
-  private
-
-  # ログインフォームのURLを取得
-  def login_poster_url(user)
-    Rails.application.routes.url_helpers.login_poster_user_url(
-      user,
-      host: 'https://device-expansion.onrender.com'
-    )
-  end
