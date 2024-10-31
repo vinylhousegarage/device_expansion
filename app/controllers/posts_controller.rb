a076fc35ef5405d20baca47d77ab544dcca9efcd
vinylhousegarage <vinylhousegarage@gmail.com>
2024-10-31 21:06:24 +0900

    Replace logger.debug with stdout in new action PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def new
-    logger.debug "Session user_id: #{session[:user_id]}"
+    $stdout.puts "Session user_id: #{session[:user_id]}"
     @user = User.find_by(id: session[:user_id])
     @post = Post.new
     @posts = Post.by_user(@user.id)
