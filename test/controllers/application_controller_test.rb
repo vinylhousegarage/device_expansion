5048eae9e4be01e1639c54e86d42eab949de7315
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 14:55:16 +0900

    Correct path helper for find_params_id test in ApplicationControllerTest

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -16,7 +16,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
 
   # find_params_id メソッドのテスト
   test "find_params_id should find user based on params[:id]" do
-    get login_poster_redirect_user(id: @user.id)
+    get login_poster_redirect_user_path(id: @user.id)
     assert_response :success
     json_response = JSON.parse(response.body)
     assert_equal login_poster_redirect_user_path(id: @user.id), json_response["redirect_url"]
