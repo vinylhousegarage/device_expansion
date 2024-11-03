f05a3bf32eb8327c270306b18bdac4d0333d43a0
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 17:45:45 +0900

    Update PostsNewViewTest to verify shared/user_info partial with user data

--- test/views/posts/new_view_test.rb
+++ test/views/posts/new_view_test.rb
@@ -3,17 +3,22 @@ require 'test_helper'
 class PostsNewViewTest < ActionView::TestCase
   setup do
     @post = Post.new
+    @user = users(:first_poster)
+    @posts = Post.where(user_id: @user.id)
   end
 
   test 'new post form renders correctly' do
     render template: 'posts/new'
 
-    assert_select 'form[action=?][method=?]', posts_path, 'post' do
+    assert_select 'form' do
       assert_select 'input[type=text][name=?]', 'post[name]'
       assert_select 'input[type=number][name=?]', 'post[amount]'
       assert_select 'input[type=text][name=?]', 'post[address]'
       assert_select 'input[type=text][name=?]', 'post[tel]'
       assert_select 'input[type=text][name=?]', 'post[others]'
     end
+
+    assert_select 'b', text: /#{@user.name}さんの登録件数：\d+/
+    assert_select 'b', text: /#{@user.name}さんの合計金額：\d+円/
   end
 end
