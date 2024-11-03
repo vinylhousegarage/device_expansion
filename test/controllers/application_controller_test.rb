e286a3f2a5e6d2995340aeca8b263357183b8f7d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 14:39:34 +0900

    Update find_params_id test to use login_poster_user_path in ApplicationControllerTest

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -11,7 +11,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     post login_poster_user_path(id: @user.id)
     assert_response :success
     json_response = JSON.parse(response.body)
-    assert_equal @user.id, json_response["user_id"]
+    assert_equal new_post_path, json_response["redirect_url"]
   end
 
   # find_params_id メソッドのテスト
@@ -19,6 +19,6 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     get login_poster_redirect_user(id: @user.id)
     assert_response :success
     json_response = JSON.parse(response.body)
-    assert_equal @user.id, json_response["user_id"]
+    assert_equal login_poster_user_path(id: @user.id), json_response["redirect_url"]
   end
 end
