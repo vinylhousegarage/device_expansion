41777909b4455b59a6eb7ffb51bba2e7976c21c0
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 14:49:22 +0900

    Rename find_params_id test path to login_poster_redirect_user in ApplicationControllerTest

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -19,6 +19,6 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     get login_poster_redirect_user(id: @user.id)
     assert_response :success
     json_response = JSON.parse(response.body)
-    assert_equal login_poster_user_path(id: @user.id), json_response["redirect_url"]
+    assert_equal login_poster_redirect_user_path(id: @user.id), json_response["redirect_url"]
   end
 end
