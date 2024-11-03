0e9724419fd18fecd8d58ef08e2539a4440bf6c9
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 11:38:05 +0900

    Rename find_user_id method to find_params_user in UsersControllerTest

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -68,7 +68,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_includes response.body, @user.name
   end
 
-  # find_user_idメソッドのテスト
+  # find_params_idメソッドのテスト
   test "find_params_id should find user based on params[:id]" do
     get login_poster_redirect_user_path(id: @user.id)
     assert_response :success
