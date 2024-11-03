863810d4d01ded4030daa0c7b387a867da86119e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 16:47:19 +0900

    Add view tests for shared/_user_info partial to verify user info rendering

---
+++ test/views/shared/_user_info_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SharedUserInfoPartialTest < ActionView::TestCase
+  def setup
+    @user = users(:first_poster)
+    @posts = posts(:first_post, :second_post)
+  end
+
+  # 登録件数をテスト
+  test 'renders the user name with post count' do
+    render partial: 'shared/user_info', locals: { user: @user, posts: @posts }
+    assert_includes rendered, "#{@user.name}さんの登録件数：#{@posts.count}"
+  end
+
+  # 合計金額をテスト
+  test 'renders the user name with total amount' do
+    render partial: 'shared/user_info', locals: { user: @user, posts: @posts }
+    total_amount = @posts.sum(&:amount)
+    formatted_amount = ActiveSupport::NumberHelper.number_to_currency(total_amount, unit: '円', delimiter: ',', format: '%n%u', precision: 0)
+    assert_includes rendered, "#{@user.name}さんの合計金額：#{formatted_amount}"
+  end
+end
