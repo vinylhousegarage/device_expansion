c58f4077d9fc313b51c0ddf0294c144d74c838df
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-03 19:43:38 +0900

    Update PostsNewViewTest to consolidate checks for user post count and total amount rendering

--- test/views/shared/_user_info_test.rb
+++ test/views/shared/_user_info_test.rb
@@ -1,21 +1,16 @@
 require 'test_helper'
 
-class SharedUserInfoPartialTest < ActionView::TestCase
-  def setup
+class PostsNewViewTest < ActionView::TestCase
+  setup do
+    @post = Post.new
     @user = users(:first_poster)
     @posts = Post.where(user_id: @user.id)
   end
 
-  # 登録件数をテスト
-  test 'renders the user name with post count' do
-    render partial: 'shared/user_info', locals: { user: @user, posts: @posts }
+  test 'renders the user name with post count and total amount' do
+    render template: 'posts/new'
     assert_includes rendered, "#{@user.name}さんの登録件数：#{@posts.count}"
-  end
-
-  # 合計金額をテスト
-  test 'renders the user name with total amount' do
-    render partial: 'shared/user_info', locals: { user: @user, posts: @posts }
-    total_amount = @posts.sum(&:amount)
+    total_amount = @posts.sum(:amount)
     formatted_amount = ActiveSupport::NumberHelper.number_to_currency(total_amount, unit: '円', delimiter: ',', format: '%n%u', precision: 0)
     assert_includes rendered, "#{@user.name}さんの合計金額：#{formatted_amount}"
   end
