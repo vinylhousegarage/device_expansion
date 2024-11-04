49145f8a745afce30360ab7be897430f73f2bb8b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-04 18:48:06 +0900

    Refactor UsersControllerTest setup to assign @post using fixture posts(:first_post)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,7 +9,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:first_poster)
     @admin_user = users(:admin)
-    @post = Post.find_by(user_id: @user.id)
+    @post = posts(:first_post)
     json_sign_in_as(@user)
   end
 
