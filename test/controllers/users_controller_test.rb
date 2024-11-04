2b4ca57091d508eb0eb4f7c0509f4b4eeafdb639
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-04 15:37:25 +0900

    Update json_sign_in_as to correctly assign user ID in params[]

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -85,6 +85,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   private
 
   def json_sign_in_as(user)
-    post login_poster_user_path(user.id), params: { session: { user_id: user.id } }, as: :json
+    post login_poster_user_path(user.id), params: { id: user.id }, as: :json
   end
 end
