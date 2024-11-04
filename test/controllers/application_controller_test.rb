f6755007634c0ab98fc3a1a26b78502450eff0d4
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-04 16:20:05 +0900

    Update current_user test to send JSON request and verify redirect URL

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -8,7 +8,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
 
   # current_user メソッドのテスト
   test "current_user should return the user based on session user_id" do
-    post login_poster_user_path(id: @user.id)
+    post login_poster_user_path(id: @user.id), as: :json
     assert_response :success
     json_response = JSON.parse(response.body)
     assert_equal new_post_path, json_response["redirect_url"]
