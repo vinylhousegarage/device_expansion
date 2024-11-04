de47f8d3d3ed9ebd1af3f7dffbec94ed7adad51e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-04 16:40:19 +0900

    Consolidate json_sign_in_as method in ApplicationControllerTest for consistent test setup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -81,10 +81,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert_match post.content, response.body
     end
   end
-
-  private
-
-  def json_sign_in_as(user)
-    post login_poster_user_path(user.id), params: { id: user.id }, as: :json
-  end
 end
