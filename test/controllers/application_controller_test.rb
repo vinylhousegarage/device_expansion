d129eab041d2f70b09cbbc98477cc413611749c1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-05 10:14:37 +0900

    Update string literals to single quotes in application_controller_test for consistency

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -8,7 +8,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   end
 
   # current_user メソッドのテスト
-  test "current_user should return the user based on session user_id" do
+  test 'current_user should return the user based on session user_id' do
     post login_poster_user_path(id: @user.id), as: :json
     assert_response :success
     json_response = JSON.parse(response.body)
@@ -16,7 +16,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   end
 
   # find_params_id メソッドのテスト
-  test "find_params_id should find user based on params[:id]" do
+  test 'find_params_id should find user based on params[:id]' do
     get login_poster_redirect_user_path(id: @user.id)
     assert_response :success
     fetch_url_pattern = /fetch\(\'\/users\/#{@user.id}\/login_poster\'/
