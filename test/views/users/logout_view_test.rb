16caaa0e657d4d207199d133039c6bb8c133dc06
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-11 20:18:16 +0900

    Update PostsNewViewTest to check home button visibility for admin user role

--- test/views/users/logout_view_test.rb
+++ test/views/users/logout_view_test.rb
@@ -2,16 +2,22 @@ require 'test_helper'
 
 class PostsNewViewTest < ActionView::TestCase
   setup do
-    @admin = users(:admin)
+    @users = [users(:first_poster), users(:admin)]
   end
 
   test 'renders logout form' do
-    render template: 'users/logout'
-    assert_select 'h3', 'おつかれ様でした'
+    @users.each do |user|
+      @user = user
 
-    if @admin.name == "集計担当"
-      assert_select 'form[action=?][method=?]', new_user_path, 'get' do
-        assert_select 'button', '初期画面へ戻る'
+      render template: 'users/logout'
+      assert_select 'h3', 'おつかれ様でした'
+
+      if @user.name == "集計担当"
+        assert_select 'form[action=?][method=?]', new_user_path, 'get' do
+          assert_select 'button', '初期画面へ戻る'
+        end
+      else
+        assert_select 'form[action=?][method=?]', new_user_path, 'get', count: 0
       end
     end
   end
