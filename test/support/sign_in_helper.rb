dfb48fb4828d8b0c3c7b47a937b94e3c85d80986
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-13 13:05:28 +0900

    Set login_poster_user_path as the default path in SignInHelper

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -1,5 +1,5 @@
 module SignInHelper
-  def sign_in_as(user, path, params: { id: user.id }, as: :html, method: :post)
+  def sign_in_as(user, path = login_poster_user_path(user.id), params: { id: user.id }, as: :html, method: :post)
     send(method, path, params: params, as: as)
     assert_response :success
   end
