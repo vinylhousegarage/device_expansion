447d2d75384ad1423435070616d9cbff055a168e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-13 11:17:55 +0900

    Rename module to LoginPosterJsonHelper in json_sign_in_helper.rb

--- test/support/json_sign_in_helper.rb
+++ test/support/json_sign_in_helper.rb
@@ -1,5 +1,5 @@
-module JsonSignInHelper
-  def json_sign_in_as(user)
+module LoginPosterJsonHelper
+  def login_poster_as_json(user)
     post login_poster_user_path(user.id), params: { id: user.id }, as: :json
   end
 end
