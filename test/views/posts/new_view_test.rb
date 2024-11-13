d5222629324c7f3877ae5a284ddd10c5905646e1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-13 09:28:51 +0900

    Add session setup for admin user role test in PostsNewViewTest

--- test/views/posts/new_view_test.rb
+++ test/views/posts/new_view_test.rb
@@ -2,22 +2,20 @@ require 'test_helper'
 
 class PostsNewViewTest < ActionDispatch::IntegrationTest
   setup do
-    @users = [users(:first_poster), users(:admin)]
+    @user = users(:first_poster)
+    session[:id] = @user.id
+    get new_post_path
   end
-
   test 'renders user-info section with correct content' do
-    @users.each do |user|
-      session[:id] = user.id
       get new_post_path
       assert_select 'div#user-info' do
-        assert_select 'b', text: "#{user.name}さんの登録件数：#{user.posts.count}件"
-        assert_select 'b', text: "#{user.name}さんの合計金額：#{number_to_currency(user.posts.sum(:amount), unit: '円', delimiter: ',', format: '%n%u', precision: 0)}"
+        assert_select 'b', text: "#{@user.name}さんの登録件数：#{@user.posts.count}件"
+        assert_select 'b', text: "#{@user.name}さんの合計金額：#{number_to_currency(@user.posts.sum(:amount), unit: '円', delimiter: ',', format: '%n%u', precision: 0)}"
       end
     end
   end
 
   test 'displays new registration form with correct fields' do
-    session[:id] = user.id
     get new_post_path
     assert_select 'h3', text: '新規登録'
     assert_select 'form' do
@@ -31,38 +29,51 @@ class PostsNewViewTest < ActionDispatch::IntegrationTest
   end
 
   test 'renders session ID and confirmation button' do
-    @users.each do |user|
-      session[:id] = user.id
-      get new_post_path
-      assert_select 'p', text: "sessionID: #{user.id}"
-      assert_select 'table' do
-        assert_select 'td', text: '集計を確認する'
-        assert_select 'td' do
-          assert_select 'form[action=?][method=?]', user_path(user), 'get' do
-            assert_select 'button', '確認'
-          end
+    assert_select 'p', text: "sessionID: #{@user.id}"
+    assert_select 'table' do
+      assert_select 'td', text: '集計を確認する'
+      assert_select 'td' do
+        assert_select 'form[action=?][method=?]', user_path(@user), 'get' do
+          assert_select 'button', '確認'
         end
       end
     end
   end
 
-  test 'displays appropriate navigation buttons based on user role' do
-    @users.each do |user|
-      session[:id] = user.id
-      get new_post_path
-      if user.name == '集計担当'
-        assert_select 'table' do
-          assert_select 'b', text: '登録状況へ戻る　'
-          assert_select 'form[action=?][method=?]', users_path, 'get' do
-            assert_select 'button', '戻る'
-          end
+  test 'displays appropriate navigation buttons based on poster user role' do
+    if @user.name == '集計担当'
+      assert_select 'table' do
+        assert_select 'b', text: '登録状況へ戻る　'
+        assert_select 'form[action=?][method=?]', users_path, 'get' do
+          assert_select 'button', '戻る'
         end
-      else
-        assert_select 'table' do
-          assert_select 'b', text: '作業を終了する　'
-          assert_select 'form[action=?][method=?]', logout_users_path, 'post' do
-            assert_select 'button', '終了'
-          end
+      end
+    else
+      assert_select 'table' do
+        assert_select 'b', text: '作業を終了する　'
+        assert_select 'form[action=?][method=?]', logout_users_path, 'post' do
+          assert_select 'button', '終了'
+        end
+      end
+    end
+  end
+
+  test 'displays appropriate navigation buttons based on admin user role' do
+    @user = users(:admin)
+    session[:id] = @user.id
+    get new_post_path
+    if @user.name == '集計担当'
+      assert_select 'table' do
+        assert_select 'b', text: '登録状況へ戻る　'
+        assert_select 'form[action=?][method=?]', users_path, 'get' do
+          assert_select 'button', '戻る'
+        end
+      end
+    else
+      assert_select 'table' do
+        assert_select 'b', text: '作業を終了する　'
+        assert_select 'form[action=?][method=?]', logout_users_path, 'post' do
+          assert_select 'button', '終了'
         end
       end
     end
