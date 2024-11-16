074931082b29cdb3e3dc667935556b49e1d9aad7
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-16 10:02:22 +0900

    Adjust indentation to comply with Rubocop in SignInHelper

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -1,6 +1,6 @@
 module SignInHelper
   def sign_in_as(user, path = login_poster_user_path(user.id), params: { id: user.id }, as: :html, method: :post)
-  send(method, path, params:, as:)
-    assert_response :success
+    send(method, path, params:, as:)
+      assert_response :success
   end
 end
