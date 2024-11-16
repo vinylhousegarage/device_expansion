59f10a56ec61b1f648ca7cbc0c3cc9d4e0978172
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-16 15:56:43 +0900

    Refactor UsersIndexViewTest to use user.posts directly

--- test/views/users/index_view_test.rb
+++ test/views/users/index_view_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class UsersIndexViewTest < ActionView::TestCase
-  def assert_user_data(user, user_posts)
+  def assert_user_data(user)
     assert_select 'td', text: user.name
-    assert_select 'td', text: "#{user_posts.count}件　"
-    assert_select 'td', text: "#{number_with_delimiter(user_posts.sum(:amount))} 円　"
+    assert_select 'td', text: "#{user.posts.count}件　"
+    assert_select 'td', text: "#{number_with_delimiter(user.posts.sum(:amount))} 円　"
   end
 
   def assert_form_action(action, method, button_text)
@@ -14,19 +14,16 @@ class UsersIndexViewTest < ActionView::TestCase
   end
 
   test 'renders index form' do
-    @users = [users(:first_poster), users(:second_poster)]
-    @user_posts = Post.where(user: @users)
+    poster_user = users(:first_poster)
+    admin_user = users(:admin)
 
     render template: 'users/index'
-    assert_select 'h3', '登録状況'
-    assert_select 'button', '詳細'
-    assert_select 'td', text: "#{@user_posts.count}件　"
-    assert_select 'td', text: "#{number_with_delimiter(@user_posts.sum(:amount))} 円　"
 
-    @users.each do |user|
-      assert_user_data(user, @user_posts)
-      assert_form_action(user_path(user.id), 'get', '詳細')
-    end
+    assert_user_data(poster_user)
+    assert_form_action(user_path(poster_user.id), 'get', '詳細')
+
+    assert_user_data(admin_user)
+    assert_form_action(user_path(admin_user.id), 'get', '詳細')
 
     assert_form_action(new_post_path, 'get', '　参加　')
     assert_form_action(new_user_path, 'get', '　戻る　')
