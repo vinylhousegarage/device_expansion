3d62782873fb89293d816414f3bced10cc0d03b3
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-16 13:27:06 +0900

    Set default locale to Japanese and update I18n load path

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :ja
+
     config.assets.paths << Rails.root.join('app/assets/builds')
     config.assets.compile = false
     config.public_file_server.enabled = true
