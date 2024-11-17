dd04b6aef864a8ef5de8814625ea454697659c66
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-17 12:19:33 +0900

    Refactor UsersIndexViewTest to an Integration Test using DELETE method

--- test/views/users/index_view_test.rb
+++ test/views/users/index_view_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersIndexViewTest < ActionView::TestCase
+class UsersIndexViewTest < ActionDispatch::IntegrationTest
   def assert_total_heading(posts)
     assert_select 'table' do
       assert_select 'td', text: '　合　　計　　'
