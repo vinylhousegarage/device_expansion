fefd766b534a9216c93084960eeda84cfedbb8ed
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 14:50:09 +0900

    Add debug output to QrCodeGeneratorServiceTest

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -6,6 +6,9 @@ class QrCodeGeneratorServiceTest < ActiveSupport::TestCase
     url = LoginPosterUrlGeneratorService.generate_login_poster_url(user)
     svg = QrCodeGeneratorService.generate_for_login_poster(user)
 
+    Rails.logger.debug "Generated URL: #{url}"
+    Rails.logger.debug "Generated SVG: #{svg}"
+
     assert svg.include?('<svg')
     assert svg.include?('</svg>')
     assert svg.include?(url)
