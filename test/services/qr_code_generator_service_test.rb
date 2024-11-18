97258e8b4e0c35a2b74bc48768b82c723f645968
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 15:33:00 +0900

    Remove assert svg.include?(url) from QrCodeGeneratorServiceTest

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -6,13 +6,7 @@ class QrCodeGeneratorServiceTest < ActiveSupport::TestCase
     url = LoginPosterUrlGeneratorService.generate_login_poster_url(user)
     svg = QrCodeGeneratorService.generate_for_login_poster(user)
 
-    # Debugging logs
-    Rails.logger.debug "Expected URL: #{url}"
-    Rails.logger.debug "Generated SVG: #{svg}"
-
-    # Asserts with meaningful messages
-    assert_not_nil url, "URL should not be nil"
-    assert_not_nil svg, "SVG should not be nil"
-    assert svg.include?(url), "Expected SVG to include URL, but URL was: #{url}"
+    assert svg.include?('<svg')
+    assert svg.include?('</svg>')
   end
 end
