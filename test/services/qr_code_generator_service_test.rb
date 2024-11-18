0012c80e8a83e2fd52766bb67bcbae2e96521354
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 12:54:48 +0900

    Rename method to generate_login_poster_url in QRCodeGeneratorTest

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QRCodeGeneratorTest < ActiveSupport::TestCase
   test 'should generate QR code SVG for login poster' do
     user = users(:first_poster)
-    url = LoginPosterURLGenerator.generate(user)
+    url = LoginPosterURLGenerator.generate_login_poster_url(user)
     svg = QRCodeGenerator.generate_for_login_poster(user)
 
     assert svg.include?('<svg')
