5f0495e9a533be7a113a4f34a748b1ddc11b0ac3
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 11:08:43 +0900

    Remove unused method from models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,38 +16,4 @@ class User < ApplicationRecord
   USERS = %w[投稿者１ 投稿者２ 投稿者３ 投稿者４ 投稿者５ 集計担当].freeze
   # すべてのユーザーをデータベースから取得するスコープを定義
   scope :users, -> { where(name: USERS) }
-
-  # セッションからユーザーを取得
-  def self.find_from_session(session)
-    user_id = session[:user_id]
-    find_by(id: user_id) if user_id.present?
-  end
-
-  # QRコード生成メソッドを実行
-  def generate_qr_code_for_login_poster_redirect
-    login_poster_redirect_url = generate_login_poster_redirect_url
-    generate_qr_code_for_login_poster_redirect_svg(login_poster_redirect_url)
-  end
-
-  private
-
-  # login_poster_redirect のURLを取得
-  def generate_login_poster_redirect_url
-    Rails.application.routes.url_helpers.login_poster_redirect_user_url(
-      self,
-      host: 'https://device-expansion.onrender.com'
-    )
-  end
-
-  # QRコードをSVG形式で生成
-  def generate_qr_code_for_login_poster_redirect_svg(url)
-    qrcode = RQRCode::QRCode.new(url)
-    qrcode.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 3,
-      standalone: true
-    )
-  end
 end
