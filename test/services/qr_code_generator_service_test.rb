3fe108c4bd58b9f919a8b23c47643c4b2d8ea43e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 14:31:14 +0900

    Rename method name in QrCodeGeneratorServiceTest

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class QrCodeGeneratorServiceTest < ActiveSupport::TestCase
   test 'should generate QR code SVG for login poster' do
     user = users(:first_poster)
-    url = LoginPosterUrlGenerator.generate_login_poster_url(user)
-    svg = QrCodeGenerator.generate_for_login_poster(user)
+    url = LoginPosterUrlGeneratorService.generate_login_poster_url(user)
+    svg = QrCodeGeneratorService.generate_for_login_poster(user)
 
     assert svg.include?('<svg')
     assert svg.include?('</svg>')
