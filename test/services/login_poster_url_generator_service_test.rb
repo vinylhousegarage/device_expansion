901bd6e635c4b9f6efef796523f0f83beb9c71fc
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 12:53:59 +0900

    Rename method to generate_login_poster_url in LoginPosterURLGeneratorTest

--- test/services/login_poster_url_generator_service_test.rb
+++ test/services/login_poster_url_generator_service_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class LoginPosterURLGeneratorTest < ActiveSupport::TestCase
   test 'should generate correct login poster redirect URL' do
     user = users(:first_poster)
-    url = LoginPosterURLGenerator.generate(user)
+    url = LoginPosterURLGenerator.generate_login_poster_url(user)
 
     expected_url = Rails.application.routes.url_helpers.login_poster_redirect_user_url(
       user,
