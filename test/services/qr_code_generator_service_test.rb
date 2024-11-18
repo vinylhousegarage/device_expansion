31b80c8e0cb65bbebc90e92c30521a72c1a13f9b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 12:19:51 +0900

    Separate QRCodeGeneratorTest into QRCodeGeneratorTest

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class QRCodeGeneratorTest < ActiveSupport::TestCase
   test 'should generate QR code SVG for login poster' do
     user = users(:first_poster)
+    url = LoginPosterURLGenerator.generate(user)
     svg = QRCodeGenerator.generate_for_login_poster(user)
 
     assert svg.include?('<svg')
     assert svg.include?('</svg>')
+    assert svg.include?(url)
   end
 end
