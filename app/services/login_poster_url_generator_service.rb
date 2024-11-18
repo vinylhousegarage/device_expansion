e8ea3cff9fbd951200efccb12bc6f50285de10d0
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 14:12:56 +0900

    Rename class name LoginPosterUrlGeneratorService

--- app/services/login_poster_url_generator_service.rb
+++ app/services/login_poster_url_generator_service.rb
@@ -1,4 +1,4 @@
-class LoginPosterURLGeneratorService
+class LoginPosterUrlGeneratorService
   def self.generate_login_poster_url(user)
     Rails.application.routes.url_helpers.login_poster_redirect_user_url(
       user,
