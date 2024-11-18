8d87e762dad3211965c6d5a284a403cef5f3da31
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 12:29:23 +0900

    Remove login_poster_url_generator_service.rb from test/services

--- test/services/login_poster_url_generator_service.rb
+++
@@ -1,8 +0,0 @@
-class LoginPosterURLGenerator
-  def self.generate(user)
-    Rails.application.routes.url_helpers.login_poster_redirect_user_url(
-      user,
-      host: 'https://device-expansion.onrender.com'
-    )
-  end
-end
