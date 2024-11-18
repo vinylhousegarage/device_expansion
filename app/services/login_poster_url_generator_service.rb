135f36f6dffd142ef25d7bc915bad6216e05334a
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 12:50:59 +0900

    Rename method to generate_login_poster_url in LoginPosterURLGenerator

--- app/services/login_poster_url_generator_service.rb
+++ app/services/login_poster_url_generator_service.rb
@@ -1,5 +1,5 @@
 class LoginPosterURLGenerator
-  def self.generate(user)
+  def self.generate_login_poster_url(user)
     Rails.application.routes.url_helpers.login_poster_redirect_user_url(
       user,
       host: 'https://device-expansion.onrender.com'
