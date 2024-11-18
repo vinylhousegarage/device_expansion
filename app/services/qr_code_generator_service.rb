74f257d9b7f8b03c73a74ea70316de3b5ff20cf9
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 12:37:02 +0900

    Separate QRCodeGenerator into QRCodeGenerator

--- app/services/qr_code_generator_service.rb
+++ app/services/qr_code_generator_service.rb
@@ -1,28 +1,21 @@
 class QRCodeGenerator
   def self.generate_for_login_poster(user)
-    login_poster_redirect_url = generate_login_poster_redirect_url(user)
-    generate_qr_code_svg(login_poster_redirect_url)
+    url = LoginPosterURLGenerator.generate(user)
+    generate_qr_code_svg(url)
   end
 
-  private
+  class << self
+    private
 
-  # login_poster_redirect のURLを取得
-  def self.generate_login_poster_redirect_url(user)
-    Rails.application.routes.url_helpers.login_poster_redirect_user_url(
-      user,
-      host: 'https://device-expansion.onrender.com'
-    )
-  end
-
-  # QRコードをSVG形式で生成
-  def self.generate_qr_code_svg(url)
-    qrcode = RQRCode::QRCode.new(url)
-    qrcode.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 3,
-      standalone: true
-    )
+    def generate_qr_code_svg(url)
+      qrcode = RQRCode::QRCode.new(url)
+      qrcode.as_svg(
+        offset: 0,
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 3,
+        standalone: true
+      )
+    end
   end
 end
