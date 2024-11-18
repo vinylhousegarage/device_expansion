7b23c2f0fe14a6bb2dd04398c0cf00394d3ed07b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 15:44:38 +0900

    Remove url variable from QrCodeGeneratorServiceTest

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class QrCodeGeneratorServiceTest < ActiveSupport::TestCase
   test 'should generate QR code SVG for login poster' do
     user = users(:first_poster)
-    url = LoginPosterUrlGeneratorService.generate_login_poster_url(user)
     svg = QrCodeGeneratorService.generate_for_login_poster(user)
 
     assert svg.include?('<svg')
