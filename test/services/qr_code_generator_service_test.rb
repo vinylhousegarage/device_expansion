6a456ebf042854114298626b0ea019459db5a2fc
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-18 15:13:41 +0900

    Refactor QrCodeGeneratorServiceTest for debug

--- test/services/qr_code_generator_service_test.rb
+++ test/services/qr_code_generator_service_test.rb
@@ -6,11 +6,13 @@ class QrCodeGeneratorServiceTest < ActiveSupport::TestCase
     url = LoginPosterUrlGeneratorService.generate_login_poster_url(user)
     svg = QrCodeGeneratorService.generate_for_login_poster(user)
 
-    Rails.logger.debug "Generated URL: #{url}"
+    # Debugging logs
+    Rails.logger.debug "Expected URL: #{url}"
     Rails.logger.debug "Generated SVG: #{svg}"
 
-    assert svg.include?('<svg')
-    assert svg.include?('</svg>')
-    assert svg.include?(url)
+    # Asserts with meaningful messages
+    assert_not_nil url, "URL should not be nil"
+    assert_not_nil svg, "SVG should not be nil"
+    assert svg.include?(url), "Expected SVG to include URL, but URL was: #{url}"
   end
 end
