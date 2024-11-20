ee346584a83585b45576be4066b0f4b26fa4c853
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-20 19:09:53 +0900

    Refactor new action of PostsController to use service class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def new
-    statistics_service = UserPostsStatsService.new
-    
-    @user = current_user
+    @current_user = current_user
     @post = Post.new
-    @user_posts = @user.posts
+    posts_stats = PostsStatsService.new
+    @total_posts_count = post_stats.total_posts_count
+    @total_posts_amount = post_stats.total_posts_amount
+    user_posts_stats = UserPostsStatsService.new
+    @user_stats = user_posts_stats.user_stats_for_id(session[:user_id])
   end
 end
