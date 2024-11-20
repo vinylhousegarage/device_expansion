f2e04bd9c140eddd321a581d5d2518fc31668c5f
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-20 14:30:47 +0900

    Rename instance variable to @user_stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   # 投稿者一覧を表示
   def index
+    posts_stats = PostsStatsService.new
+    @total_posts_count = post_stats.total_posts_count
+    @total_posts_amount = post_stats.total_posts_amount
     user_posts_stats = UserPostsStatsService.new
     @user_stats = user_posts_stats.user_stats
-    @total_posts_count = stats_service.total_posts_count
-    @total_posts_amount = stats_service.total_posts_amount
   end
 
   # 個人別投稿一覧を表示
