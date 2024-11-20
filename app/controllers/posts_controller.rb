fe0e37d7ec020458a072d3508fc141834caa4d44
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-20 19:40:51 +0900

    Remove PostsStatsService from new action of PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,6 @@ class PostsController < ApplicationController
   def new
     @current_user = current_user
     @post = Post.new
-    posts_stats = PostsStatsService.new
-    @total_posts_count = post_stats.total_posts_count
-    @total_posts_amount = post_stats.total_posts_amount
     user_posts_stats = UserPostsStatsService.new
     @user_stats = user_posts_stats.user_stats_for_id(session[:user_id])
   end
