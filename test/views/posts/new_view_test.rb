eac4eea0b78d75d7749cb3d2864a9ab2dc2aa6fd
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-21 14:56:26 +0900

    Split setup in PostsNewViewTest

--- test/views/posts/new_view_test.rb
+++ test/views/posts/new_view_test.rb
@@ -4,20 +4,31 @@ class PostsNewViewTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::NumberHelper
 
   def setup
-    @user = users(:first_poster)
-    sign_in_as(@user)
-    @users = [users(:first_poster), users(:admin)]
-    @user_stats = [
-      { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
-      { user_name: '集計担当', post_count: 2, post_amount: 50_000 }
-    ]
-    @total_posts_count = 4
-    @total_posts_amount = 58_000
+    initialize_user
+    initialize_user_stats
+    stub_services
+  end
 
+  private
+
+  def initialize_user
+    @user = users(:first_poster)
+    sign_in_as(@user, as: :json)
+  end
+
+  def initialize_user_stats
+    @user_stats = [
+      { user_name: '投稿者１', post_count: 2, post_amount: 8000 },
+      { user_name: '投稿者２', post_count: 3, post_amount: 12000 }
+    ]
+  end
+
+  def stub_services
     UserPostsStatsService.stub :new, OpenStruct.new(user_stats: @user_stats) do
-      get new_post_path
     end
   end
+
+  public
 
   def access_new_post_page(user)
     sign_in_as(user)
