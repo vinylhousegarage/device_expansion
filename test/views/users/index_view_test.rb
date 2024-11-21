1f39c37e746472af0ff8c96e7befcba66ce29603
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-21 12:13:03 +0900

    Refactor UsersIndexViewTest to use stub

--- test/views/users/index_view_test.rb
+++ test/views/users/index_view_test.rb
@@ -3,21 +3,31 @@ require 'test_helper'
 class UsersIndexViewTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::NumberHelper
 
-  def assert_total_heading(posts)
+  def setup
+    @users = [users(:first_poster), users(:admin)]
+    @user_stats = [
+      { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
+      { user_name: '集計担当', post_count: 2, post_amount: 50_000 }
+    ]
+    @total_posts_count = 4
+    @total_posts_amount = 58_000
+  end
+
+  def assert_total_heading(total_count, total_amount)
     assert_select 'table' do
       assert_select 'td', text: '　合　　計　　'
-      assert_select 'td', text: "#{posts.count}件　"
-      assert_select 'td', text: "#{number_with_delimiter(posts.sum(:amount))} 円　"
+      assert_select 'td', text: "#{total_count}件　"
+      assert_select 'td', text: "#{number_with_delimiter(total_amount)} 円　"
       assert_form_action(posts_path, 'get', '詳細')
     end
   end
 
-  def assert_user_index(user)
+  def assert_user_index(stat)
     assert_select 'table' do
-      assert_select 'td', text: user.name
-      assert_select 'td', text: "#{user.posts.count}件　"
-      assert_select 'td', text: "#{number_with_delimiter(user.posts.sum(:amount))} 円　"
-      assert_form_action(user_path(user.id), 'get', '詳細')
+      assert_select 'td', text: stat[:user_name]
+      assert_select 'td', text: "#{stat[:post_count]}件　"
+      assert_select 'td', text: "#{number_with_delimiter(stat[:post_amount])} 円　"
+      assert_form_action(user_path(stat[:user_name]), 'get', '詳細')
     end
   end
 
@@ -36,20 +46,21 @@ class UsersIndexViewTest < ActionDispatch::IntegrationTest
   end
 
   test 'renders index view with all elements' do
-    @users = User.all
-    @posts = Post.all
+    PostsStatsService.stub :new, OpenStruct.new(total_posts_count: @total_posts_count, total_posts_amount: @total_posts_amount) do
+      UserPostsStatsService.stub :new, OpenStruct.new(user_stats: @user_stats) do
+        get users_path
 
-    get users_path
+        assert_total_heading(@total_posts_count, @total_posts_amount)
 
-    assert_total_heading(@posts)
+        @user_stats.each do |stat|
+          assert_user_index(stat)
+        end
 
-    @users.each do |user|
-      assert_user_index(user)
+        assert_button('参加', new_post_path, 'get')
+        assert_button('戻る', new_user_path, 'get')
+        assert_button('削除', reset_database_users_path, 'delete')
+      end
     end
-
-    assert_button('参加', new_post_path, 'get')
-    assert_button('戻る', new_user_path, 'get')
-    assert_button('削除', reset_database_users_path, 'delete')
   end
 
   test 'reset database from index view' do
