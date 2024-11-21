ee357d324c9dc40cabb0a598744055ed6ab05392
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-21 13:55:59 +0900

    Refactor UserInfoAssertions to use service class

--- test/support/user_info_assertions.rb
+++ test/support/user_info_assertions.rb
@@ -1,9 +1,12 @@
 module UserInfoAssertions
   def assert_user_info(user)
+    user_stat = @user_stats.find { |stat| stat[:user_name] == user.name }
+
     assert_select 'div#user-info' do
-      assert_select 'b', text: user.name + "さんの登録件数：#{user.posts.count}件"
-      total_amount = formatted_total_amount(user.posts.sum(:amount))
-      assert_select 'b', text: user.name + "さんの合計金額：#{total_amount}"
+      assert_select 'b', text: "#{user_stat[:user_name]}さんの登録件数：#{user_stat[:post_count]}件"
+
+      total_amount = formatted_total_amount(user_stat[:post_amount])
+      assert_select 'b', text: "#{user_stat[:user_name]}さんの合計金額：#{total_amount}"
     end
   end
 
