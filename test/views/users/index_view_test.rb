485bf3bb5588a1bb42889eb189238b9023dd6e2b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 19:38:43 +0900

    Rename user_stats to all_users_stats and user_stats_for_id to user_stats_by_id in UsersIndexViewTest

--- test/views/users/index_view_test.rb
+++ test/views/users/index_view_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class UsersIndexViewTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::NumberHelper
 
-  POST_STATS_STRUCT = Struct.new(:total_posts_count, :total_posts_amount)
-  USER_STATS_STRUCT = Struct.new(:user_stats)
+  POSTS_STATS_STRUCT = Struct.new(:total_posts_count, :total_posts_amount)
+  USERS_STATS_STRUCT = Struct.new(:user_stats)
 
   def setup
     initialize_user
-    initialize_post_stats
-    initialize_user_stats
+    initialize_posts_stats
+    initialize_all_users_stats
   end
 
   def assert_total_heading(total_count, total_amount)
@@ -52,12 +52,12 @@ class UsersIndexViewTest < ActionDispatch::IntegrationTest
     @users = [users(:first_poster), users(:admin)]
   end
 
-  def initialize_post_stats
+  def initialize_posts_stats
     @total_posts_count = 4
     @total_posts_amount = 58_000
   end
 
-  def initialize_user_stats
+  def initialize_all_users_stats
     @user_stats = [
       { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
       { user_name: '集計担当', post_count: 2, post_amount: 50_000 }
@@ -65,16 +65,16 @@ class UsersIndexViewTest < ActionDispatch::IntegrationTest
   end
 
   test 'renders index view with all elements' do
-    post_stats_stub = POST_STATS_STRUCT.new(@total_posts_count, @total_posts_amount)
-    user_stats_stub = USER_STATS_STRUCT.new(@user_stats)
-    PostsStatsService.stubs(:new).returns(post_stats_stub)
-    UserPostsStatsService.stubs(:new).returns(user_stats_stub)
+    posts_stats_stub = POSTS_STATS_STRUCT.new(@total_posts_count, @total_posts_amount)
+    users_stats_stub = USERS_STATS_STRUCT.new(@all_users_stats)
+    PostsStatsService.stubs(:new).returns(posts_stats_stub)
+    UserPostsStatsService.stubs(:new).returns(users_stats_stub)
     get users_path
 
     assert_total_heading(@total_posts_count, @total_posts_amount)
 
-    @user_stats.each do |stat|
-      assert_user_index(stat)
+    @all_user_stats.each do |stats|
+      assert_user_index(stats)
     end
 
     assert_button('参加', new_post_path, 'get')
