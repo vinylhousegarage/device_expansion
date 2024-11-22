759fc10ff7c96af929a93c5bfe27193edd8f6051
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 19:11:22 +0900

    Rename user_stats to all_users_stats and user_stats_for_id to user_stats_by_id in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,6 @@ class PostsController < ApplicationController
     @current_user = current_user
     @post = Post.new
     user_posts_stats = UserPostsStatsService.new
-    @user_stats = user_posts_stats.user_stats(session[:user_id])
+    @user_stats_by_id = user_posts_stats.user_stats_by_id(session[:user_id])
   end
 end
