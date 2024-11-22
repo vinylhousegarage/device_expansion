dc193e503af0c711e76c6f74ef18d14ce5d3318a
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 14:22:49 +0900

    Replace stub with stubs in ApplicationControllerActionTest

--- test/controllers/application_controller_action_test.rb
+++ test/controllers/application_controller_action_test.rb
@@ -26,7 +26,7 @@ class ApplicationControllerActionTest < ActionController::TestCase
 
   def stub_services
     user_stats_stub = USER_STATS_STRUCT.new(@user_stats)
-    UserPostsStatsService.stub(:new, user_stats_stub) do
+    UserPostsStatsService.stubs(:new, user_stats_stub) do
       get new_post_path
     end
   end
