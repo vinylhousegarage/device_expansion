081efd6a8700cff39d0573c33f9caaaf9a291bfb
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 13:51:10 +0900

    Add user_info test to UsersShowViewTest

--- test/views/users/show_view_test.rb
+++ test/views/users/show_view_test.rb
@@ -5,6 +5,36 @@ class UsersShowViewTest < ActionDispatch::IntegrationTest
 
   def setup
     @users = [users(:first_poster), users(:admin)]
+    initialize_user
+    initialize_user_stats
+    stub_services
+  end
+
+  private
+
+  def initialize_user
+    @user = users(:first_poster)
+    sign_in_as(@user)
+  end
+
+  def initialize_user_stats
+    @user_stats = [
+      { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
+      { user_name: '投稿者２', post_count: 3, post_amount: 12_000 }
+    ]
+  end
+
+  def stub_services
+    user_stats_stub = USER_STATS_STRUCT.new(@user_stats)
+    UserPostsStatsService.stub(:new, user_stats_stub) do
+      get new_post_path
+    end
+  end
+
+  test 'renders user-info section with correct content' do
+    @users.each do |user|
+      assert_user_info(user)
+    end
   end
 
   test 'displays headers in show template for different user roles' do
