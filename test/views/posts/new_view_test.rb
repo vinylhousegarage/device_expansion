ed56ab7f7f2573bf33f75bd55e50cb36f7dae9a5
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 19:36:08 +0900

    Rename user_stats to all_users_stats and user_stats_for_id to user_stats_by_id in PostsNewViewTest

--- test/views/posts/new_view_test.rb
+++ test/views/posts/new_view_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class PostsNewViewTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::NumberHelper
 
-  USER_STATS_STRUCT = Struct.new(:user_stats)
+  USERS_STATS_STRUCT = Struct.new(:user_stats)
 
   def setup
     initialize_user
-    initialize_user_stats
+    initialize_all_users_stats
     stub_services
   end
 
@@ -41,15 +41,15 @@ class PostsNewViewTest < ActionDispatch::IntegrationTest
   end
 
   def initialize_user_stats
-    @user_stats = [
+    @all_users_stats = [
       { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
       { user_name: '投稿者２', post_count: 3, post_amount: 12_000 }
     ]
   end
 
   def stub_services
-    user_stats_stub = USER_STATS_STRUCT.new(@user_stats)
-    UserPostsStatsService.stubs(:new).returns(user_stats_stub)
+    users_stats_stub = USERS_STATS_STRUCT.new(@all_users_stats)
+    UserPostsStatsService.stubs(:new).returns(users_stats_stub)
     get new_post_path
   end
 
