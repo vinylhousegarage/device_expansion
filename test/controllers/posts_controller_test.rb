6ab67f6be05ced5d8f5524ae0bf61766ef9edfef
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 14:19:05 +0900

    Refactor setup in PostsControllerTest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,10 +5,38 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  # Structを定数に定義
+  USER_STATS_STRUCT = Struct.new(:user_stats)
+
   # セッションデータを設定
-  setup do
+  def setup
+    initialize_user
+    initialize_user_stats
+    stub_services
+  end
+
+  private
+
+  # 初期化を設定
+  def initialize_user
     @user = users(:first_poster)
-    sign_in_as(@user, as: :json)
+    sign_in_as(@user)
+  end
+
+  # スタブデータを設定
+  def initialize_user_stats
+    @user_stats = [
+      { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
+      { user_name: '投稿者２', post_count: 3, post_amount: 12_000 }
+    ]
+  end
+
+  # サービスクラスをスタブ化
+  def stub_services
+    user_stats_stub = USER_STATS_STRUCT.new(@user_stats)
+    UserPostsStatsService.stubs(:new, user_stats_stub) do
+      get new_post_path
+    end
   end
 
   # newアクションをテスト
