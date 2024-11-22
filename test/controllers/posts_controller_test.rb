4360d781067b751938aa8b4f7ac0b1586ca16c3a
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 19:28:25 +0900

    Rename user_stats to all_users_stats and user_stats_for_id to user_stats_by_id in PostsControllerTest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -11,7 +11,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   # セッションデータを設定
   def setup
     initialize_user
-    initialize_users_stats
+    initialize_all_users_stats
   end
 
   private
@@ -23,8 +23,8 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   # スタブデータを設定
-  def initialize_users_stats
-    @users_stats = [
+  def initialize_all_users_stats
+    @all_users_stats = [
       { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
       { user_name: '投稿者２', post_count: 3, post_amount: 12_000 }
     ]
@@ -32,11 +32,11 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
   # newアクションをテスト
   test 'should get new post form' do
-    users_stats_stubs = USERS_STATS_STRUCT.new(@users_stats)
+    users_stats_stubs = USERS_STATS_STRUCT.new(@all_users_stats)
     UserPostsStatsService.stubs(:new).returns(users_stats_stubs)
     get new_post_path
     assert_response :success
     users_posts_stats = UserPostsStatsService.new
-    assert_equal @users_stats, users_posts_stats.users_stats
+    assert_equal @all_users_stats, users_posts_stats.all_users_stats
   end
 end
