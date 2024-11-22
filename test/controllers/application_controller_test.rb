b2ab7ebe0b58224bb294b301e25f0f4b2125df1e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-22 15:10:04 +0900

    Merge ApplicationControllerActionTest and ApplicationControllerIntegrationTest into ApplicationControllerTest

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,9 +1,45 @@
 require 'test_helper'
 
-class ApplicationControllerIntegrationTest < ActionDispatch::IntegrationTest
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  # Structを定数に定義
+  USER_STATS_STRUCT = Struct.new(:user_stats)
+
+  # セッションデータを設定
   def setup
+    initialize_user
+    initialize_user_stats
+    stub_services
+  end
+
+  private
+
+  # 初期化を設定
+  def initialize_user
     @user = users(:first_poster)
     sign_in_as(@user)
+  end
+
+  # ユーザーのスタブデータを設定
+  def initialize_user_stats
+    @user_stats = [
+      { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
+      { user_name: '投稿者２', post_count: 3, post_amount: 12_000 }
+    ]
+  end
+
+  # 投稿のスタブデータを設定
+  def stub_services
+    user_stats_stub = USER_STATS_STRUCT.new(@user_stats)
+    UserPostsStatsService.stubs(:new, user_stats_stub) do
+      get new_post_path
+    end
+  end
+
+  # current_userメソッドのテスト
+  test 'current_user should return the user based on session user_id' do
+    get new_post_path
+    assert_response :success
+    assert_equal @user.id, @controller.current_user
   end
 
   # find_params_idメソッドのテスト
