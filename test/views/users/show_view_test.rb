d7296864f75e7242527a8ffb334d619755af9800
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 12:04:51 +0900

    Refactor UsersShowViewTest to use StatsMockSetupHelper

--- test/views/users/show_view_test.rb
+++ test/views/users/show_view_test.rb
@@ -3,61 +3,37 @@ require 'test_helper'
 class UsersShowViewTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::NumberHelper
 
-  USERS_STATS_STRUCT = Struct.new(:user_stats)
-
-  def setup
-    initialize_user
-    initialize_all_users_stats
-    stub_services
-  end
-
-  private
-
-  def initialize_user
+  setup do
     @user = users(:first_poster)
-    @users = [users(:first_poster), users(:admin)]
+    @admin_user = users(:admin)
+    @users = [@user, @admin_user]
+    mock_user_posts_stats = UserPostsStatsService.new
+    @mock_user_stats_by_id = mock_user_posts_stats.user_stats_by_id(@user.id)
+    @mock_all_users_stats = mock_user_posts_stats.mock_all_users_stats(@user, @admin_user)
     sign_in_as(@user)
-  end
-
-  def initialize_all_users_stats
-    @all_users_stats = [
-      { user_name: '投稿者１', post_count: 2, post_amount: 8_000 },
-      { user_name: '投稿者２', post_count: 3, post_amount: 12_000 }
-    ]
-  end
-
-  def stub_services
-    users_stats_stub = USERS_STATS_STRUCT.new(@all_users_stats)
-    UserPostsStatsService.stubs(:new).returns(users_stats_stub)
-    get user_path
+    get user_path(@user)
   end
 
   test 'renders user-info section with correct content' do
-    @users.each do |user|
-      assert_user_info(user)
-    end
+    assert_user_info(user)
   end
 
   test 'displays headers in show template for different user roles' do
-    @users.each do |user|
-      assert_select 'h3', text: "#{user.name}さんの登録一覧"
-
-      assert_select 'table' do
-        assert_select 'th', text: 'No.'
-        assert_select 'th', text: '氏名'
-        assert_select 'th', text: '金額'
-      end
+    assert_select 'h3', text: "#{@user.name}さんの登録一覧"
+    assert_select 'table' do
+      assert_select 'th', text: 'No.'
+      assert_select 'th', text: '氏名'
+      assert_select 'th', text: '金額'
     end
   end
 
   test 'displays user posts with correct details in show template' do
-    @users.each do |user|
-      user.posts.each_with_index do |user_post, index|
+    @mock_all_users_stats.each_with_index do |stats, index|
         assert_select 'tr' do
           assert_select 'td', text: (index + 1).to_s
-          assert_select 'td', text: user_post.name
-          assert_select 'td', text: "　#{number_with_delimiter(user_post.amount)} 円　"
-          assert_select 'form[action=?][method=?]', post_path(user_post), 'get' do
+          assert_select 'td', text: stats.user_name
+          assert_select 'td', text: "　#{number_with_delimiter(stats.post_amount)} 円　"
+          assert_select 'form[action=?][method=?]', post_path(stats.user_id), 'get' do
             assert_select 'button', '詳細'
           end
         end
@@ -71,7 +47,7 @@ class UsersShowViewTest < ActionDispatch::IntegrationTest
         assert_select 'button', '新規登録へ戻る'
       end
 
-      next if user.name == '投稿者１'
+      if user.name == '集計担当'
 
       assert_select 'form[action=?][method=?]', users_path, 'get' do
         assert_select 'button', '登録状況へ戻る'
