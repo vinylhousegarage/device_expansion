e0ce3f06f2be7a7d10dc18121153169b9e06e702
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 12:46:11 +0900

    Refactor mock_posts_stats helper to use Struct in ApplicationControllerTest

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,19 +1,13 @@
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:first_poster)
+    @mock_user_stats_by_id = mock_user_stats_by_id(@user)
     sign_in_as(@user)
   end
 
   # current_userメソッドのテスト
   test 'current_user should return the user based on session user_id' do
-    mock_user_posts_stats = UserPostsStatsService::UserStat.new(
-      user: @user,
-      user_id: @user.id,
-      user_name: @user.name,
-      post_count: 3,
-      post_amount: 5_000
-    )
-    UserPostsStatsService.any_instance.stubs(:user_stats_by_id).returns(mock_user_posts_stats)
+    UserPostsStatsService.any_instance.stubs(:user_stats_by_id).returns(@mock_user_stats_by_id)
     get new_post_path
     assert_response :success
     assert_match @user.name, response.body
