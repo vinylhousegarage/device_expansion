77e793c51f6f59ac6739a3b0385ed106dee1487f
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 12:38:39 +0900

    Refactor StatsMcokSetupHelper

--- test/support/stats_mock_setup_helper.rb
+++ test/support/stats_mock_setup_helper.rb
@@ -1,9 +1,9 @@
 module StatsMockSetupHelper
   def mock_posts_stats
-    {
-      total_posts_count: 8,
-      total_posts_amount: 32_000
-    }
+    PostsStatsService::UserStat.new(
+      total_posts_count: 7,
+      total_posts_amount: 35_000
+    )
   end
 
   def mock_user_stats_by_id(user)
@@ -22,15 +22,15 @@ module StatsMockSetupHelper
         user: user,
         user_id: 1,
         user_name: '投稿者１',
-        post_count: 5,
-        post_amount: 17_000
+        post_count: 3,
+        post_amount: 15_000
       ),
       UserPostsStatsService::UserStat.new(
         user: admin_user,
         user_id: 6,
         user_name: '集計担当',
-        post_count: 3,
-        post_amount: 15_000
+        post_count: 4,
+        post_amount: 20_000
       )
     ]
   end
