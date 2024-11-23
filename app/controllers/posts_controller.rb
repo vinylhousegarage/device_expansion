2b1d3377ee34e80a956b6b156bfefb4dc8cc0661
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 19:10:45 +0900

    Change session[:user_id] to integer in posts#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   def new
     @current_user = current_user
     @post = Post.new
-    @user_stats_by_id = user_posts_stats.user_stats_by_id(session[:user_id])
+    @user_stats_by_id = user_posts_stats.user_stats_by_id(session[:user_id].to_i)
   end
 end
