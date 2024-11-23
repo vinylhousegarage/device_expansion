3d88fbec50daf0802b15e3d2967104c63aeb85ee
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 19:09:01 +0900

    Remove service class from PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def new
     @current_user = current_user
     @post = Post.new
-    user_posts_stats = UserPostsStatsService.new
     @user_stats_by_id = user_posts_stats.user_stats_by_id(session[:user_id])
   end
 end
