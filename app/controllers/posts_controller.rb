4476230aa00fed4a8864c3402c480a00b6b1d63c
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 19:39:54 +0900

    Add UserPostsStatsService instance to posts#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def new
     @current_user = current_user
     @post = Post.new
+    user_posts_stats = UserPostsStatsService.new
     @user_stats_by_id = user_posts_stats.user_stats_by_id(session[:user_id].to_i)
   end
 end
