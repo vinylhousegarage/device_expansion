d056227fa04091ad0676d8384fcd30109db1ee9d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 19:08:30 +0900

    Remove service class from UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
   # 投稿者一覧を表示
   def index
-    posts_stats = PostsStatsService.new
     @total_posts_count = posts_stats.total_posts_count
     @total_posts_amount = posts_stats.total_posts_amount
-    user_posts_stats = UserPostsStatsService.new
     @all_users_stats = user_posts_stats.all_users_stats
   end
 
   # 個人別投稿一覧を表示
   def show
     @user = find_params_id
-    user_posts_stats = UserPostsStatsService.new
     @all_users_stats = user_posts_stats.all_users_stats
     @user_stats_by_id = user_posts_stats.user_stats_by_id(params[:id].to_i)
   end
