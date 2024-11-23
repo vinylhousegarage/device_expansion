eb78a6b579f3e3ff07125bc71ce0c11592be6f37
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-23 16:47:58 +0900

    Refactor StatsMockSetupHelper to use data from fixtures

--- test/support/stats_mock_setup_helper.rb
+++ test/support/stats_mock_setup_helper.rb
@@ -11,8 +11,8 @@ module StatsMockSetupHelper
       user: user,
       user_id: user.id,
       user_name: user.name,
-      post_count: 2,
-      post_amount: 6_000
+      post_count: user.posts.size,
+      post_amount: user.posts.sum(:amount)
     )
   end
 
@@ -20,17 +20,17 @@ module StatsMockSetupHelper
     [
       UserPostsStatsService::UserStat.new(
         user: user,
-        user_id: 1,
-        user_name: '投稿者１',
-        post_count: 3,
-        post_amount: 15_000
+        user_id: user.id,
+        user_name: user.name,
+        post_count: user.posts.size,
+        post_amount: user.posts.sum(:amount)
       ),
       UserPostsStatsService::UserStat.new(
         user: admin_user,
-        user_id: 6,
-        user_name: '集計担当',
-        post_count: 4,
-        post_amount: 20_000
+        user_id: admin_user.id,
+        user_name: admin_user.name,
+        post_count: admin_user.posts.size,
+        post_amount: admin_user.posts.sum(:amount)
       )
     ]
   end
