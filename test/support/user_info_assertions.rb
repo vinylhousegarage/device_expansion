ad68f764023a94dc0da6a9e6f8b71de892483e3b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-24 12:33:21 +0900

    Remove div#user-info from UserInfoAssertions

--- test/support/user_info_assertions.rb
+++ test/support/user_info_assertions.rb
@@ -3,12 +3,10 @@ module UserInfoAssertions
     user_stats = @mock_all_users_stats.find { |stat| stat.user_name == user.name }
     raise "User stats not found for user: #{user.name}" unless user_stats
 
-    assert_select 'div#user-info' do
-      assert_select 'b', text: "#{user_stats.user_name}さんの登録件数：#{user_stats.post_count}件"
+    assert_select 'b', text: "#{user_stats.user_name}さんの登録件数：#{user_stats.post_count}件"
 
-      total_amount = formatted_total_amount(user_stats.post_amount)
-      assert_select 'b', text: "#{user_stats.user_name}さんの合計金額：#{total_amount}"
-    end
+    total_amount = formatted_total_amount(user_stats.post_amount)
+    assert_select 'b', text: "#{user_stats.user_name}さんの合計金額：#{total_amount}"
   end
 
   private
