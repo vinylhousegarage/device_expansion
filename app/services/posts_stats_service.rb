e746b9ac0af1d9c71c1ccfca9d91f4e04967adeb
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-24 09:42:04 +0900

    Rename method in PostsStatsService

--- app/services/posts_stats_service.rb
+++ app/services/posts_stats_service.rb
@@ -1,11 +1,11 @@
 class PostsStatsService
   UserStat = Struct.new(:total_posts_count, :total_posts_amount, keyword_init: true)
 
-  def total_posts_count
+  def calculate_posts_count
     Post.count
   end
 
-  def total_posts_amount
+  def calculate_posts_amount
     Post.sum(:amount)
   end
 end
