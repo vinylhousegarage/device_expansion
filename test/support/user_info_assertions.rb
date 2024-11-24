61ebe2d5cc5e0d796ab1326a3b795c1dc1eaabca
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-24 13:52:19 +0900

    Add 'div#_user_info' to assert_info method in UserInfoAssertions

--- test/support/user_info_assertions.rb
+++ test/support/user_info_assertions.rb
@@ -2,11 +2,11 @@ module UserInfoAssertions
   def assert_user_info(user)
     user_stats = @mock_all_users_stats.find { |stat| stat.user_name == user.name }
     raise "User stats not found for user: #{user.name}" unless user_stats
-
-    assert_select 'b', text: "#{user_stats.user_name}さんの登録件数：#{user_stats.post_count}件"
-
-    total_amount = formatted_total_amount(user_stats.post_amount)
-    assert_select 'b', text: "#{user_stats.user_name}さんの合計金額：#{total_amount}"
+    assert_select 'div#_user_info' do
+      assert_select 'b', text: "#{user_stats.user_name}さんの登録件数：#{user_stats.post_count}件"
+      total_amount = formatted_total_amount(user_stats.post_amount)
+      assert_select 'b', text: "#{user_stats.user_name}さんの合計金額：#{total_amount}"
+    end
   end
 
   private
