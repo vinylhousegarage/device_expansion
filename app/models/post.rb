b8f31d5fca9f9ebba66456b29b358b2a52080a29
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-25 20:31:50 +0900

    Add user_post_index to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,13 @@ class Post < ApplicationRecord
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   before_validation :normalize_amount
 
+  def user_post_index
+    user.posts.order(:created_at).pluck(:id).index(self.id) + 1
+  end
+
   private
 
   def normalize_amount
     self.amount = nil if amount.to_s.strip.empty?
   end
-
-  scope :by_user, ->(user_id) { where(user_id:) }
 end
