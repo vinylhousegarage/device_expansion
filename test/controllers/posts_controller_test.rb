c779dfa3a0ebf7d221bef44a3f37e312e6564e7b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-26 13:40:52 +0900

    Add find_post_by_id method test in PostsControllerTest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,4 +1,27 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  # テスト用の初期データを設定
+  def setup
+    @user = users(:first_poster)
+    @post = posts(:first_post)
+    sign_in_as(@user)
+  end
+
+  # find_posts_by_paramsメソッドをテスト
+  test 'should successfully delete the correct post' do
+    assert_difference 'Post.count', -1 do
+      delete post_path(@post)
+    end
+
+    assert_response :redirect
+    follow_redirect!
+    assert_match 'Post was successfully deleted.', response.body
+
+    assert_nil Post.find_by(id: @post.id)
+  end
 end
