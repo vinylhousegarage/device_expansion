f3c7637ac6523bf6629978cb072cd6c3897f2a16
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-26 14:48:36 +0900

    Replace double quote with single in PostsStatsServiceTest

--- test/services/posts_stats_servic_test.rb
+++ test/services/posts_stats_servic_test.rb
@@ -6,14 +6,14 @@ class PostsStatsServiceTest < ActiveSupport::TestCase
   end
 
   # calculate_posts_countメソッドをテスト
-  test "calculate_posts_count returns correct count" do
+  test 'calculate_posts_count returns correct count' do
     expected_count = Post.count
     result = @service.calculate_posts_count
     assert_equal expected_count, result
   end
 
   # calculate_posts_countメソッドをテスト
-  test "calculate_posts_amount returns correct sum of amount" do
+  test 'calculate_posts_amount returns correct sum of amount' do
     expected_sum = Post.sum(:amount)
     result = @service.calculate_posts_amount
     assert_equal expected_sum, result
