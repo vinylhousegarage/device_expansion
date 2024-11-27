6d7f56f2cd5139899ca6d587b7db053d76492a2f
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-27 13:16:14 +0900

    Rename FlashAssertions method

--- test/support/flash_assertions.rb
+++ test/support/flash_assertions.rb
@@ -1,14 +1,14 @@
 module FlashAssertions
-  def assert_flash(key, expected_message)
+  def assert_flash_set(key, expected_message)
     assert flash[key], "flash[:#{key}] not set"
     assert_equal expected_message, flash[key], "flash[:#{key}] mismatch"
   end
 
-  def assert_flash_present(key)
+  def assert_flash_key_present(key)
     assert flash[key], "flash[:#{key}] is nil"
   end
 
-  def assert_flash_absent(key)
+  def assert_flash_key_absent(key)
     assert_nil flash[key], "flash[:#{key}] is not nil (value: #{flash[key]})"
   end
 end
