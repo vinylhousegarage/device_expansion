3e69168f72c3426ad80363d97054d4d1f288f929
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-27 10:10:09 +0900

    Refactor user_path test to use first_poster in PostDestroyServiceTest

--- test/services/post_destroy_service_test.rb
+++ test/services/post_destroy_service_test.rb
@@ -9,21 +9,25 @@ class PostDestroyServiceTest < ActiveSupport::TestCase
 
   # ユーザーに紐づく全ての投稿が削除された場合のリダイレクトをテスト
   test 'should return :new_post_path when post is destroyed and user has no posts left' do
+    @user = users(:second_poster)
+    @post = posts(:sixth_post)
     @post.destroy
-    result = @service.call
+    result = PostDestroyService.new(@post).call
     assert_equal :new_post_path, result[:path]
   end
 
   # ユーザーに紐づく投稿が存在する場合のリダイレクトをテスト
   test 'should return :user_path when post is destroyed and user has other posts' do
+    @user = users(:first_poster)
+    @post = posts(:first_post)
     @post.destroy
-    result = @service.call
+    result = PostDestroyService.new(@post).call
     assert_equal :user_path, result[:path]
   end
 
   # 投稿の削除が失敗した場合のリダイレクトをテスト
   test 'should return :new_post_path when post destruction fails' do
-    @post.destroy
+    @post.stubs(:destroy).returns(false)
     result = @service.call
     assert_equal :new_post_path, result[:path]
   end
