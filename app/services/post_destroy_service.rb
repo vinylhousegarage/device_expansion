d5eb31122d5e8928972c26fd441ac2644473878d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-27 16:52:44 +0900

    Refactor call method to use params: @user in PostDestroyService

--- app/services/post_destroy_service.rb
+++ app/services/post_destroy_service.rb
@@ -7,12 +7,12 @@ class PostDestroyService
   def call
     if @post.destroy
       if @user.posts.count.zero?
-        { path: :new_post_path, type: :notice, message_key: 'notices.all_deleted' }
+        { path: :new_post_path, type: :notice, message_key: 'notices.all_deleted', params: nil }
       else
-        { path: :user_path, type: :notice, message_key: 'notices.post_deleted' }
+        { path: :user_path, type: :notice, message_key: 'notices.post_deleted', params: @user }
       end
     else
-      { path: :new_post_path, type: :alert, message_key: 'alerts.delete_failed' }
+      { path: :new_post_path, type: :alert, message_key: 'alerts.delete_failed', params: nil }
     end
   end
 end
