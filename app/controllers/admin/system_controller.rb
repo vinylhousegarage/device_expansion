5d371c9efc7f52158ab767aa1b4adcd68019b3fc
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-27 19:44:52 +0900

    Add admin/system_controller.rb in app/controllers

---
+++ app/controllers/admin/system_controller.rb
@@ -0,0 +1,32 @@
+module Admin
+  class SystemController < ApplicationController
+    before_action :ensure_admin_user
+
+    # 管理者ログイン
+    def login
+      session[:user_id] = User.admin_users.first&.id
+      redirect_to admin_dashboard_path
+    end
+
+    # 管理者ログアウト
+    def logout
+      session[:user_id] = nil
+      redirect_with_notice(root_path, 'notices.logged_out')
+    end
+
+    # データベースリセット
+    def reset_database
+      DataResetService.call
+      redirect_with_notice(admin_dashboard_path, 'notices.data_reset')
+    end
+
+    private
+
+    # 管理者権限チェック
+    def ensure_admin_user
+      unless User.admin_users.pluck(:id).include?(session[:user_id])
+        redirect_with_alert(root_path, 'errors.unauthorized_access')
+      end
+    end
+  end
+end
