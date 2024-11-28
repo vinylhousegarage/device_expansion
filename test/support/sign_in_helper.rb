3f186b9f2656eac97bb0cbf45f8d3f2764ff10af
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-28 12:22:39 +0900

    Add admin_sign_in_as to SignInHelper

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -3,4 +3,9 @@ module SignInHelper
     send(method, path, params:, as:)
     assert_response :success
   end
+
+  def admin_sign_in_as(user, path = admin_session_path(user.id), params: { id: user.id }, as: :html, method: :post)
+  send(method, path, params:, as:)
+  assert_response :success
+  end
 end
