b4006615162ad105b7a159630925be212671fdd1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 07:27:48 +0900

    Refactor users route to qr_code in LoginFormViewTest

--- test/views/qr_code/login_form_view_test.rb
+++ test/views/qr_code/login_form_view_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class LoginFormViewTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:first_poster)
-    post login_form_user_path(@user.id)
+    post login_form_qr_code_path(@user.id)
     assert_response :success
   end
 
   test 'should display login_form page with correct elements' do
     assert_select 'svg', true, 'QRコードが表示されていません'
-    assert_select 'a[href=?]', login_poster_redirect_user_path(@user.id), text: nil
+    assert_select 'a[href=?]', login_poster_redirect_qr_code_path(@user.id), text: nil
     assert_select 'form[action=?][method=?]', new_user_path, 'get'
   end
 
   test 'should navigate to login_poster_redirect path successfully' do
-    get login_poster_redirect_user_path(@user.id)
+    get login_poster_redirect_qr_code_path(@user.id)
     assert_response :success
   end
 
