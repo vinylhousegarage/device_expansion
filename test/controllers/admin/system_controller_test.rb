6815ef71db1cc238465585ac2974dba92309098c
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-28 11:02:48 +0900

    Refactor admin routes in Admin::SystemControllerTest

--- test/controllers/admin/system_controller_test.rb
+++ test/controllers/admin/system_controller_test.rb
@@ -2,19 +2,9 @@ require 'test_helper'
 
 class Admin::SystemControllerTest < ActionDispatch::IntegrationTest
   def setup
-    post admin_login_path
+    post admin_session_path
     assert_response :redirect
     assert_redirected_to users_path
-  end
-
-  # admin/system#logout のパスとフラッシュをテスト
-  test 'should successfully post to logout admin path' do
-    delete admin_logout_path
-    assert_response :redirect
-    assert_redirected_to root_path
-    follow_redirect!
-    assert_response :success
-    assert_flash_set(I18n.t('notices.data_reset'))
   end
 
   # admin/system#reset_database のパスとフラッシュをテスト
