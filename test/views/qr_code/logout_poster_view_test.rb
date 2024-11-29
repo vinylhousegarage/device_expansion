a5ee976aca803399db77889b7e43595db7f10234
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 09:23:13 +0900

    Add logout_poster_view_test.rb

---
+++ test/views/qr_code/logout_poster_view_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class QrCodeLogoutPosterViewTest < ActionView::TestCase
+  def setup
+    @user = users(:first_poster)
+  end
+
+  test 'renders logout_poster page with farewell message' do
+    render template: 'qr_code#logout_poster'
+    assert_select 'h3', 'おつかれ様でした'
+  end
+end
