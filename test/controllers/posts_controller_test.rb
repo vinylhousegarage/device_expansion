ec33ea14843338d768ffbed56d3010bcafb689c4
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 12:46:39 +0900

    Add create action test to PostsControllerTest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -24,4 +24,21 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
     assert_nil Post.find_by(id: @post.id)
   end
+
+  # createアクションの正常をテスト
+  test 'should create post successfully' do
+    assert_difference 'Post.count', 1 do
+      post posts_path, params: { post: { name: 'テストユーザー', amount: 3_000, address: '東京都', tel: '08012345678', others: '備考' } }
+    end
+    assert_redirected_to new_post_path
+  end
+
+  # createアクションの異常をテスト
+  test 'should not create post with invalid attributes' do
+    assert_no_difference 'Post.count' do
+      post posts_path, params: { post: { name: '', amount: nil, address: '', tel: 'abc123', others: '' } }
+    end
+    assert_response :unprocessable_entity
+    assert_template :new
+  end
 end
