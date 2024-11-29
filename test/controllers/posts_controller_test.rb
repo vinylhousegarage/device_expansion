747b9d817a0811659c2eab56c55c86681810f5fc
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 17:35:07 +0900

    Refactor amount argument to integer in create action test of PostsControllerTest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -32,7 +32,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_equal @user.id.to_s, session[:user_id], 'Session user_id is not correctly set'
 
     assert_difference 'Post.count', 1 do
-      post posts_path, params: { post: { name: 'テストユーザー', amount: 3_000, address: '東京都', tel: '08012345678', others: '備考' } }
+      post posts_path, params: { post: { name: 'テストユーザー', amount: 3000, address: '東京都', tel: '08012345678', others: '備考' } }
     end
     assert_redirected_to new_post_path
   end
