5974f8c7fba74fe52501ed707c840cd5f1d48339
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 09:41:47 +0900

    Replace 'qr_code#logout_poster' with 'qr_code/logout_poster' in QrCodeLogoutPosterViewTest

--- test/views/qr_code/logout_poster_view_test.rb
+++ test/views/qr_code/logout_poster_view_test.rb
@@ -3,11 +3,10 @@ require 'test_helper'
 class QrCodeLogoutPosterViewTest < ActionView::TestCase
   def setup
     @user = users(:first_poster)
-    @controller.session[:user_id] = @user.id
   end
 
   test 'renders logout_poster page with farewell message' do
-    render template: 'qr_code#logout_poster'
+    render template: 'qr_code/logout_poster'
     assert_select 'h3', 'おつかれ様でした'
   end
 end
