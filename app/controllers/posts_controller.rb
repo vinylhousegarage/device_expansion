9ec6171698a8e1c416042d3ebdfaf86b09c456af
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 12:51:52 +0900

    Add create action and post_params to PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,23 @@ class PostsController < ApplicationController
     @user_stats_by_id = user_posts_stats.user_stats_by_id(session[:user_id].to_i)
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @post = find_post_by_params
     @user = @post.user
     result = PostDestroyService.new(@post).call
     redirect_with_flash(send(result[:path], *Array(result[:params])), result[:type], result[:message_key])
+  end
+
+  def post_params
+    params.require(:post).permit(:name, :amount, :address, :tel, :others)
   end
 end
