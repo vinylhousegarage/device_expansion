b4a0952edb8ffb91e4403a20d79d470993e78c1b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-29 13:04:35 +0900

    Rename current_user to set_current_user in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :find_user_by_id
+  helper_method :set_current_user, :find_user_by_id
 
   # セッションの user_id に基づいて現在のユーザーを取得
-  def current_user
+  def set_current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
