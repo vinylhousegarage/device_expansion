604b91ca61e128817725d04fc4888c64f596e8c4
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-30 17:24:09 +0900

    Add PostsEditViewTest

--- test/views/posts/edit_view_test.rb
+++ test/views/posts/edit_view_test.rb
@@ -5,7 +5,7 @@ class PostsEditViewTest < ActionDispatch::IntegrationTest
     @user = users(:first_poster)
     @admin_user = users(:admin)
     @post = posts(:second_post)
-    @mock_user_post_index = 5
+    @mock_user_post_index = @post.user_post_index
     @mock_user_stats_by_id = mock_user_stats_by_id(@user)
     sign_in_as(@user)
   end
@@ -24,22 +24,17 @@ class PostsEditViewTest < ActionDispatch::IntegrationTest
       assert_select 'input[name=?]', 'post[others]'
     end
 
-    assert_select 'p', text: "登録No. #{@user_post_index}"
+    assert_select 'p', text: "登録No. #{@mock_user_post_index}"
 
     assert_select 'form[action=?][method=?]', post_path(@post), 'post'
     assert_select 'form[action=?][method=?]', new_post_path, 'post'
   end
 
   test 'edit view renders additional button for aggregation user' do
-    # モックデータで条件分岐部分をテスト
-    @user_stats_by_id = OpenStruct.new(user_name: '集計担当')
-
-    # リクエスト
     get edit_post_path(@post)
-
     assert_response :success
+    admin_sign_in_as(@admin_user)
 
-    # 集計担当用のボタン確認
     assert_select 'form[action=?][method=?]', users_path, 'post'
     assert_select 'td', text: /登録状況へ戻る/
   end
