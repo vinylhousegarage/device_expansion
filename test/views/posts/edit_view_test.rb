3373a3d4dc1a473a81bb817cd3caacb3d9210d2c
vinylhousegarage <vinylhousegarage@gmail.com>
2024-11-30 19:36:54 +0900

    Refactor variable setup for button test in PostsEditViewTest

--- test/views/posts/edit_view_test.rb
+++ test/views/posts/edit_view_test.rb
@@ -34,6 +34,8 @@ class PostsEditViewTest < ActionDispatch::IntegrationTest
   end
 
   test 'edit view renders additional button for aggregation user' do
+    @adimin_user = users(:admin)
+    admin_sign_in_as(@admin_user)
     @post = posts(:third_post)
     get edit_post_path(@post)
     assert_response :success
