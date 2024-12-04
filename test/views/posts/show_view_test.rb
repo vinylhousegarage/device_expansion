49375b75741c18d4fa6ef0c31fa92638668e1075
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-04 11:55:16 +0900

    Separate show view test for poster_user in PostsShowViewTest

--- test/views/posts/show_view_test.rb
+++ test/views/posts/show_view_test.rb
@@ -10,20 +10,22 @@ class PostsShowViewTest < ActionDispatch::IntegrationTest
     @post_amount = mock_user_stats_by_id(@user).post_amount
     @user_stats_by_id = mock_user_stats_by_id(@user)
     sign_in_as(@user)
+    get post_path(@post)
   end
 
-  test 'show view renders correctly for general user' do
-    get post_path(@post)
-    assert_response :success
-
+  test 'show view renders correctly for partial' do
     assert_select 'div#_user_info', text: /#{@user.name}さんの登録件数：#{@post_count}件/
     formatted_post_amount = number_to_currency(@post_amount, unit: '円', delimiter: ',', format: "%n%u", precision: 0)
     assert_select 'div#_user_info', text: /#{@user.name}さんの合計金額：#{formatted_post_amount}/
+  end
 
+  test 'show view renders correctly for user_post_index' do
     @user_post_index = @post.user_post_index
     assert_select 'div', text: /登録番号：#{@user_post_index}/
     puts @response.body
+  end
 
+  test 'show view renders correctly for table' do
     assert_select 'table' do
       assert_select 'tr:nth-child(1) td:nth-child(1)', text: '氏名：'
       assert_select 'tr:nth-child(1) td:nth-child(2)', text: @post.name
@@ -41,7 +43,9 @@ class PostsShowViewTest < ActionDispatch::IntegrationTest
       assert_select 'tr:nth-child(5) td:nth-child(1)', text: '備考'
       assert_select 'tr:nth-child(5) td:nth-child(2)', text: @post.others
     end
+  end
 
+  test 'show view renders correctly for button' do
     assert_select 'form[action=?][method=?]', edit_post_path(@post), 'get' do
       assert_select 'button[type="submit"]', text: '投稿を編集する'
     end
