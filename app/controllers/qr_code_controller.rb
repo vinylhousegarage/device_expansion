fd91af8b3de60f02202a3c85c6587955cd891297
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-05 08:59:22 +0900

    Transfer user session resposibility from QrCodeController to SessionsController

--- app/controllers/qr_code_controller.rb
+++ app/controllers/qr_code_controller.rb
@@ -5,20 +5,4 @@ class QrCodeController < ApplicationController
     @url = LoginPosterUrlGeneratorService.generate_login_poster_url(@user)
     @svg = QrCodeGeneratorService.generate_for_login_poster(@user)
   end
-
-  # 投稿者のトップページを設定
-  def login_poster
-    session[:user_id] = params[:id]
-    render json: { redirect_url: new_post_path }
-  end
-
-  # GETルートで受けたQRコードのパスをPOSTルートに変換
-  def login_poster_redirect
-    @user = find_user_by_params
-  end
-
-  # 投稿者のログアウト
-  def logout_poster
-    session[:user_id] = nil
-  end
 end
