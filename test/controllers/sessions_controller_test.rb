9a7384923252be8fa4a4e17981f82059b17d2b9b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-05 11:53:40 +0900

    Rename class to SessionsControllerTest

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class QrCodeControllerTest < ActionDispatch::IntegrationTest
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   include ActionView::Helpers::NumberHelper
   # test "the truth" do
   #   assert true
