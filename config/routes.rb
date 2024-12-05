bbffa8cd13ad97fbf36fa7fb9f78e529a702e70a
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-05 09:12:10 +0900

    Fix syntax error in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
       post 'login_poster'
       get 'login_poster_redirect'
     end
-    collection
+    collection do
       delete 'logout_poster'
     end
   end
