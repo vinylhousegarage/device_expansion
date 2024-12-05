5fcdda3d64771092f07a3401f21f5b47290dc410
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-05 11:41:44 +0900

    Replace login_poster_qr_code_path with login_poster_session_path in LoginPosterUrlGeneratorService

--- app/services/login_poster_url_generator_service.rb
+++ app/services/login_poster_url_generator_service.rb
@@ -1,6 +1,6 @@
 class LoginPosterUrlGeneratorService
   def self.generate_login_poster_url(user)
-    Rails.application.routes.url_helpers.login_poster_redirect_qr_code_url(
+    Rails.application.routes.url_helpers.login_poster_redirect_session_url(
       user,
       host: 'https://device-expansion.onrender.com'
     )
