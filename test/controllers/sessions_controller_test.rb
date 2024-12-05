47c44b3ebae08f67a554304110b2e1ddc0efee13
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-05 11:48:33 +0900

    Transfer user session resposibility from QrCodeController to SessionsController in QrCodeControllerTest

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -20,20 +20,20 @@ class QrCodeControllerTest < ActionDispatch::IntegrationTest
 
   # '投稿者１'でのログインをテスト
   test 'should log in first_poster user' do
-    post login_poster_qr_code_path(@user), params: { id: @user.id }, as: :json
+    post login_poster_session_path(@user), params: { id: @user.id }, as: :json
     assert_response :success
     assert_equal @user.id, session[:user_id].to_i
   end
 
   # login_poster_redirectルートとリダイレクトをテスト
   test 'should route to login_poster_redirect' do
-    get login_poster_redirect_qr_code_path(@user)
+    get login_poster_redirect_session_path(@user)
     assert_response :success
   end
 
   # users#logout_poster のパスをテスト
   test 'should successfully post to logout_poster path' do
-    delete logout_poster_qr_code_index_path
+    delete logout_poster_sessions_path
     assert_response :success
   end
 end
