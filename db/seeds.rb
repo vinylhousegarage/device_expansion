dfa1c53c4a1ed4155a16c8247ff820b68aa16582
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-06 14:47:20 +0900

    Refactor user_name from poster to guest in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # 投稿者を定義
-POSTER_USERS = %w[投稿者１ 投稿者２ 投稿者３ 投稿者４ 投稿者５].freeze
+POSTER_USERS = %w[ゲスト１ ゲスト２ ゲスト３ ゲスト４ ゲスト５].freeze
 # 管理者を「集計担当」でとして定義
 ADMIN_USERS = %w[集計担当].freeze
 # 投稿者と管理者を統合
