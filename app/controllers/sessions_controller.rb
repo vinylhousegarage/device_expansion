9444e95b53cb2a68f7fd23e8e2f3533848b09afc
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-06 13:28:35 +0900

    Move login_posetr_redirect over login_poster

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
+  # GETルートで受けたQRコードのパスをPOSTルートに変換
+  def login_poster_redirect
+    @user = find_user_by_params
+  end
+
   # 投稿者のトップページを設定
   def login_poster
     session[:user_id] = params[:id]
     render json: { redirect_url: new_post_path }
-  end
-
-  # GETルートで受けたQRコードのパスをPOSTルートに変換
-  def login_poster_redirect
-    @user = find_user_by_params
   end
 
   # 投稿者のログアウト
