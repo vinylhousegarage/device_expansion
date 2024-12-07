9898becbda7fe99b3b488a82d87295bf9c676c2f
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-07 19:24:40 +0900

    Refactor setup to use by_user scope in PostsIndexViewTest

--- test/views/posts/index_view_test.rb
+++ test/views/posts/index_view_test.rb
@@ -5,9 +5,7 @@ class PostsIndexViewTest < ActionDispatch::IntegrationTest
 
   setup do
     @user = users(:first_poster)
-    @posts = Post.includes(:user).all
-    @total_posts_count = mock_posts_stats.total_posts_count
-    @total_posts_amount = mock_posts_stats.total_posts_amount
+    @posts = Post.by_user(@user)
     @user_stats_by_id = mock_user_stats_by_id(@user)
     sign_in_as(@user)
   end
@@ -46,9 +44,7 @@ class PostsIndexViewTest < ActionDispatch::IntegrationTest
     puts "Session user ID: #{session[:user_id]}"
     assert_response :redirect
 
-    @posts = [posts(:third_post), posts(:fourth_post), posts(:fifth_post)]
-    @total_post_count = @posts.size
-    @total_post_amount = @posts.sum(&:amount)
+    @posts = Post.by_user(@admin_user)
     @user_stats_by_id = mock_user_stats_by_id(@admin_user)
     puts "User stats by ID user_name: #{@user_stats_by_id.user_name}"
     get posts_path
