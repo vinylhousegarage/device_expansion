b2e835a3a0f139fc123fbabfaeb07cc6e78ba5d5
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-08 16:08:16 +0900

    Remove total detail button in UsersIndexViewTest

--- test/views/users/index_view_test.rb
+++ test/views/users/index_view_test.rb
@@ -19,7 +19,6 @@ class UsersIndexViewTest < ActionDispatch::IntegrationTest
       assert_select 'td', text: '　合　　計　　'
       assert_select 'td', text: "#{total_count}件　"
       assert_select 'td', text: "#{number_with_delimiter(total_amount)} 円　"
-      assert_form_action(posts_path, 'get', '詳細')
     end
   end
 
