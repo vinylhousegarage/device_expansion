736dcaa9189e45856dad703cdf92c91f95ad20a7
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-10 12:49:40 +0900

    Separate params logic for find_user and find_post methods in QrCodeController

--- app/controllers/qr_code_controller.rb
+++ app/controllers/qr_code_controller.rb
@@ -1,7 +1,7 @@
 class QrCodeController < ApplicationController
   # QRコードを取得し@svgに格納
   def login_form
-    @user = find_user_by_params
+    @user = find_user_by_id
     @url = LoginPosterUrlGeneratorService.generate_login_poster_url(@user)
     @svg = QrCodeGeneratorService.generate_for_login_poster(@user)
   end
