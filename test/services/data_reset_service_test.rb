ab7064727bf6dc4a4cd7c9213ae60e7ec098da21
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-11 20:34:40 +0900

    Test DataResetService for database reset, seed execution, and sequence reset

--- test/services/data_reset_service_test.rb
+++ test/services/data_reset_service_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class DataResetServiceTest < ActiveSupport::TestCase
-  test 'it calls Rails.application.load_seed' do
+  test 'it resets the database and calls Rails.application.load_seed' do
     Rails.application.expects(:load_seed).once
 
-    DataResetService.call
+    assert_difference('User.count', -User.count) do
+      assert_difference('Post.count', -Post.count) do
+        DataResetService.call
+      end
+    end
+
+    assert_equal 1, ActiveRecord::Base.connection.execute('SELECT last_value FROM users_id_seq').first['last_value']
+    assert_equal 1, ActiveRecord::Base.connection.execute('SELECT last_value FROM posts_id_seq').first['last_value']
   end
 end
