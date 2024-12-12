e49b44347a6da8af2f053ee694e4974bea90adbd
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-12 15:31:38 +0900

    Remove redundant constant definitions in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# 投稿者を定義
-POSTER_USERS = %w[ゲスト１ ゲスト２ ゲスト３ ゲスト４ ゲスト５].freeze
-
-# 管理者を定義
-ADMIN_USERS = %w[集計担当].freeze
-
-# ユーザーのリストを統合
-USERS = (POSTER_USERS + ADMIN_USERS).freeze
-
-# 初期データを作成
-USERS.each do |user_name|
+# ユーザーを定義
+User::USERS.each do |user_name|
   User.find_or_create_by!(name: user_name)
 end
