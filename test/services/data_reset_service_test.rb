42c1db756d1e4cb973e2196e86636a3851830e7b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-12 10:17:35 +0900

    Refactor setup for admin_user in DataResetServiceTest

--- test/services/data_reset_service_test.rb
+++ test/services/data_reset_service_test.rb
@@ -2,27 +2,23 @@ require 'test_helper'
 
 class DataResetServiceTest < ActiveSupport::TestCase
   setup do
-    @user = users(:first_poster)
+    @admin_user = users(:admin)
+    post admin_session_path
+    assert_response :redirect
   end
 
   test 'it resets the database and auto-increments' do
-    Rails.logger.debug { "User_count_before: #{User.count}" }
     Rails.logger.debug { "Post_count_before: #{Post.count}" }
 
     post reset_database_admin_system_path
     assert_response :redirect
 
-    assert_difference('User.count', -1) do
-      Rails.logger.debug { "User_counta_after: #{User.count}" }
+    assert_difference('Post.count', -6) do
+      Rails.logger.debug { "Post_count_after: #{Post.count}" }
 
-      assert_difference('Post.count', -2) do
-        Rails.logger.debug { "Post_count_after: #{Post.count}" }
-
-        DataResetService.call
-      end
+      DataResetService.call
     end
 
-    assert_equal 1, ActiveRecord::Base.connection.execute('SELECT last_value FROM users_id_seq').first['last_value']
     assert_equal 1, ActiveRecord::Base.connection.execute('SELECT last_value FROM posts_id_seq').first['last_value']
   end
 end
