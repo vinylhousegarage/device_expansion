1f1a7c19102a36065a0a9081eb8ecc765a781e7d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-12 09:52:46 +0900

    Add debug output for post to DataResetServiceTest

--- test/services/data_reset_service_test.rb
+++ test/services/data_reset_service_test.rb
@@ -7,8 +7,16 @@ class DataResetServiceTest < ActiveSupport::TestCase
 
   test 'it resets the database and auto-increments' do
     Rails.logger.debug { "User_count: #{User.count}" }
+
     assert_difference('User.count', -1) do
+
+      Rails.logger.debug { "User_count: #{User.count}" }
+      Rails.logger.debug { "Post_count: #{Post.count}" }
+
       assert_difference('Post.count', -2) do
+
+        Rails.logger.debug { "Post_count: #{Post.count}" }
+
         DataResetService.call
       end
     end
