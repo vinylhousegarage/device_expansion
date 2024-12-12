1be5c7a6d3482b4741ca46739c4b5ba615e149b5
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-12 10:05:04 +0900

    Add admin/system#reset_database request to DataResetServiceTest

--- test/services/data_reset_service_test.rb
+++ test/services/data_reset_service_test.rb
@@ -6,22 +6,21 @@ class DataResetServiceTest < ActiveSupport::TestCase
   end
 
   test 'it resets the database and auto-increments' do
-    Rails.logger.debug { "User_count: #{User.count}" }
+    Rails.logger.debug { "User_count_before: #{User.count}" }
+    Rails.logger.debug { "Post_count_before: #{Post.count}" }
+
+    post reset_database_admin_system_path
+    assert_response :redirect
 
     assert_difference('User.count', -1) do
-
-      Rails.logger.debug { "User_count: #{User.count}" }
-      Rails.logger.debug { "Post_count: #{Post.count}" }
+      Rails.logger.debug { "User_counta_after: #{User.count}" }
 
       assert_difference('Post.count', -2) do
-
-        Rails.logger.debug { "Post_count: #{Post.count}" }
+        Rails.logger.debug { "Post_count_after: #{Post.count}" }
 
         DataResetService.call
       end
     end
-
-
 
     assert_equal 1, ActiveRecord::Base.connection.execute('SELECT last_value FROM users_id_seq').first['last_value']
     assert_equal 1, ActiveRecord::Base.connection.execute('SELECT last_value FROM posts_id_seq').first['last_value']
