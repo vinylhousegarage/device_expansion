7dc02ffae5fb7204b1de01e1989406ac8a8549f1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-14 10:57:13 +0900

    Refactor redirect method argument in Admin::SystemControllerTest

--- test/controllers/admin/system_controller_test.rb
+++ test/controllers/admin/system_controller_test.rb
@@ -12,7 +12,7 @@ module Admin
     test 'should successfully post to reset_database path' do
       post reset_database_admin_system_path
       assert_response :redirect
-      assert_redirected_to root_path
+      assert_redirected_to users_path
       follow_redirect!
       assert_response :success
       assert_flash_set(I18n.t('notices.data_reset'))
