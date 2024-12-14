85a3c0420cb9659b0a13088a263dd24f8e61db6c
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-14 11:00:42 +0900

    Refactor redirect method argument in Admin::SessionsControllerTest

--- test/controllers/admin/sessions_controller_test.rb
+++ test/controllers/admin/sessions_controller_test.rb
@@ -5,14 +5,14 @@ module Admin
     def setup
       post admin_session_path
       assert_response :redirect
-      assert_redirected_to users_path
+      assert_redirected_to new_user_path
     end
 
     # 管理者ログアウトアクションのパスとフラッシュをテスト
     test 'should successfully post to admin logout path with flash' do
       delete admin_session_path
       assert_response :redirect
-      assert_redirected_to users_path
+      assert_redirected_to root_path
       follow_redirect!
       assert_response :success
       assert_flash_set(I18n.t('notices.logged_out'))
