98e57b646daf72a3b9a45b0f56a0f489950248bd
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-14 10:40:15 +0900

    Fix redirect route_path in Admin::SystemControllerTest

--- test/controllers/admin/system_controller_test.rb
+++ test/controllers/admin/system_controller_test.rb
@@ -5,14 +5,14 @@ module Admin
     def setup
       post admin_session_path
       assert_response :redirect
-      assert_redirected_to users_path
+      assert_redirected_to new_user_path
     end
 
     # admin/system#reset_database のパスとフラッシュをテスト
     test 'should successfully post to reset_database path' do
       post reset_database_admin_system_path
       assert_response :redirect
-      assert_redirected_to users_path
+      assert_redirected_to root_path
       follow_redirect!
       assert_response :success
       assert_flash_set(I18n.t('notices.data_reset'))
