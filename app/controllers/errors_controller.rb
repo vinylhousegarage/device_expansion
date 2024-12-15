3fd5a796eda8ab2582b1f963190b9f6ef77d8fd3
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-15 16:17:04 +0900

    Refactor errors#not_found: Use render for clarity and status handling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,5 @@
 class ErrorsController < ApplicationController
   def not_found
-    Rails.logger.warn('404 Not Found')
-    redirect_with_alert(root_path, not_found)
+    render :not_found
   end
 end
