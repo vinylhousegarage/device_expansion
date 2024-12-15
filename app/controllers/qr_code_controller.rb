f5c3c2eaff5a4c4c199aa5afed7c847a1d6a5b35
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-15 19:55:58 +0900

    Rename serviec class to QrCodeRequestUrlGeneratorService in QrCodeController

--- app/controllers/qr_code_controller.rb
+++ app/controllers/qr_code_controller.rb
@@ -2,7 +2,7 @@ class QrCodeController < ApplicationController
   # QRコードを取得し@svgに格納
   def login_form
     @user = find_user_by_id
-    @url = LoginPosterUrlGeneratorService.generate_login_poster_url(@user)
-    @svg = QrCodeGeneratorService.generate_for_login_poster(@user)
+    @url = QrCodeRequestUrlGeneratorService.generate_qr_code_request_url(@user)
+    @svg = QrCodeGeneratorService.generate_qr_code_request(@user)
   end
 end
