c9d67bd7c086ddf49da5fa5ce632ae7f2c7ee52d
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-15 20:41:49 +0900

    Add qr_code_request to QrCodeController

--- app/controllers/qr_code_controller.rb
+++ app/controllers/qr_code_controller.rb
@@ -5,4 +5,15 @@ class QrCodeController < ApplicationController
     @url = QrCodeRequestUrlGeneratorService.generate_qr_code_request_url(@user)
     @svg = QrCodeGeneratorService.generate_qr_code_request(@user)
   end
-end
+
+  # QRコードの確認
+  def qr_code_request
+    begin
+      @user = find_user_by_id
+      redirect_to login_poster_session_path(@user)
+    rescue ActionController::ParameterMissing => e
+      render :qr_code_request, status: :bad_request
+    rescue ActiveRecord::RecordNotFound
+      render :qr_code_request, status: :not_found
+    end
+  end
