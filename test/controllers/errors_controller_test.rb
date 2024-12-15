c9f113857f98fa8973f26011d03e5f3290471103
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-15 16:51:31 +0900

    Replace assert_redirect with assert_response in ErrorsControllerTest for not_found action

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -2,12 +2,9 @@ require 'test_helper'
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   # errors#not_found のパスをテスト
-  test 'should redirect to root with alert for not_found' do
+  test 'renders not_found template' do
     get '/non_existent_page'
-
-    assert_redirected_to root_path
-
-    follow_redirect!
-    assert_equal 'ページが見つかりません', flash[:alert]
+    assert_response :not_found
+    assert_template :not_found
   end
 end
