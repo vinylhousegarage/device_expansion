ca1aa14062a5cdad6dafcbf8b7268ff18fff1b22
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-16 09:38:08 +0900

    Replace login_poster assert_response from success with redirect in HomeIntroductionViewTest

--- test/views/home/introduction_view_test.rb
+++ test/views/home/introduction_view_test.rb
@@ -23,7 +23,7 @@ class HomeIntroductionViewTest < ActionDispatch::IntegrationTest
   test 'should start button for poster user' do
     @user = users(:first_poster)
     post login_poster_session_path(@user)
-    assert_response :success
+    assert_response :redirect
 
     get root_path
     assert_response :success
