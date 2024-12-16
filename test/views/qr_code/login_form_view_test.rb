d4a07403c78ac5059c1245fb6ba2959a38dec10c
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-16 09:35:45 +0900

    Replace qr_code_redirect assert_response from success with redirect in PostsControllerTest

--- test/views/qr_code/login_form_view_test.rb
+++ test/views/qr_code/login_form_view_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class QrCodeLoginFormViewTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:first_poster)
-    post login_form_qr_code_path(@user.id)
+    post login_form_qr_code_path(@user)
     assert_response :success
   end
 
   test 'should display login_form page with correct elements' do
     assert_select 'svg', true, 'QRコードが表示されていません'
-    assert_select 'a[href=?]', qr_code_request_qr_code_path(@user.id), text: nil
+    assert_select 'a[href=?]', qr_code_request_qr_code_path(@user), text: nil
     assert_select 'form[action=?][method=?]', new_user_path, 'get'
   end
 
-  test 'should navigate to login_poster_redirect path successfully' do
-    get qr_code_request_qr_code_path(@user.id)
+  test 'should navigate to qr_code_request path successfully' do
+    get qr_code_request_qr_code_path(@user)
     assert_response :redirect
   end
 
