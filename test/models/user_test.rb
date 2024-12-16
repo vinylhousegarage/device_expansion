b2f7aff15976ed5dfd4b03cdfd4d723a174b92f1
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-16 11:34:53 +0900

    Remove login_admin action test from UserTest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,10 +32,4 @@ class UserTest < ActiveSupport::TestCase
     assert_includes @user.posts, post1
     assert_includes @user.posts, post2
   end
-
-  # login_adminアクションのテスト
-  test 'should get admin user from fixture' do
-    admin_user = users(:admin)
-    assert_equal '集計担当', admin_user.name
-  end
 end
