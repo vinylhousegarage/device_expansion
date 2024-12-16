6c8fd90d55f9fad20b3975ec67413fe2c9922244
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-16 18:54:06 +0900

    Add errorhandling to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :set_current_user, :find_user_by_id
+  rescue_from ActionController::ParameterMissing, with: :handle_error
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_error
 
   # セッションの user_id に基づいて現在のユーザーを取得
   def set_current_user
@@ -28,5 +30,31 @@ class ApplicationController < ActionController::Base
   def redirect_with_alert(path, message_key)
     full_message_key = "alerts.#{message_key}"
     redirect_to path, alert: I18n.t(full_message_key)
+  end
+
+  private
+
+  # 無効なパスを検知
+  def handle_not_found
+    exception = ActionController::RoutingError.new('No route matches')
+    handle_error(exception)
+  end
+
+  # エラーハンドリングを統一
+  def handle_error(exception)
+    case exception
+    when ActionController::ParameterMissing
+      status = :bad_request
+      message = I18n.t('errors.messages.bad_request')
+    when ActiveRecord::RecordNotFound
+      status = :not_found
+      message = I18n.t('errors.messages.not_found')
+    else
+      status = :internal_server_error
+      message = I18n.t('errors.messages.internal_server_error')
+    end
+
+    Rails.logger.error "Error: #{exception.class} - #{exception.message}"
+    render template: 'application/handle_not_found', status: status, locals: { message: message }
   end
 end
