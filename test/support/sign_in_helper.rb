7a3389c59bf7f801810bf82decfb0d7da9106bf3
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-16 09:21:32 +0900

    Replace assert_response from success with redirect in SignInHelper

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -1,7 +1,7 @@
 module SignInHelper
   def sign_in_as(user, path = login_poster_session_path(user.id), params: { id: user.id }, as: :html, method: :post)
     send(method, path, params:, as:)
-    assert_response :success
+    assert_response :redirect
   end
 
   def admin_sign_in_as(user, path = admin_session_path(user.id), params: { id: user.id }, as: :html, method: :post)
