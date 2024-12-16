3904c5fbac3313ddedd0f38b04bc39408afdb40e
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-16 11:53:14 +0900

    Replace login_poster_session_path with sessions_path in HomeIntroductionViewTest

--- test/views/home/introduction_view_test.rb
+++ test/views/home/introduction_view_test.rb
@@ -22,7 +22,7 @@ class HomeIntroductionViewTest < ActionDispatch::IntegrationTest
 
   test 'should start button for poster user' do
     @user = users(:first_poster)
-    post login_poster_session_path(@user)
+    post sessions_path(@user)
     assert_response :redirect
 
     get root_path
