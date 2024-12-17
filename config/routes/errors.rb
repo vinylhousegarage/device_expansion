ebf708c2e65fe7e78871c485933b5211924dc4e2
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-17 14:10:15 +0900

    Add test simulation path to errors.rb

--- config/routes/errors.rb
+++ config/routes/errors.rb
@@ -1,4 +1,6 @@
 if Rails.env.test?
+  get '/bad_request_simulation', to: 'application#bad_request_simulation'
+  get '/not_found_simulation', to: 'application#not_found_simulation'
   get '/internal_server_error_simulation', to: 'application#internal_server_error_simulation'
 end
 
