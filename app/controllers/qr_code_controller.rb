0f497b872f526c6653d352a8632e33dd76250501
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-17 15:08:04 +0900

    Remove error handling for RecordNotFound and ParameterMissing in QrCodeController

--- app/controllers/qr_code_controller.rb
+++ app/controllers/qr_code_controller.rb
@@ -4,20 +4,15 @@ class QrCodeController < ApplicationController
     @user = find_user_by_id
     @url = QrCodeRequestUrlGeneratorService.generate_qr_code_request_url(@user)
     @svg = QrCodeGeneratorService.generate_qr_code_request(@user)
-    rescue ActiveRecord::RecordNotFound => e
-    Rails.logger.error "RecordNotFound in login_form: #{e.message}"
-    render :qr_code_request, status: :not_found
   end
 
   # QRコードの確認
   def qr_code_request
     @user = find_user_by_id
     redirect_to create_session_path(@user)
-  rescue ActionController::ParameterMissing => e
-    Rails.logger.error "ParameterMissing in qr_code_request: #{e.message}"
+  rescue ActionController::ParameterMissing
     render :qr_code_request, status: :bad_request
-  rescue ActiveRecord::RecordNotFound => e
-    Rails.logger.error "RecordNotFound in qr_code_request: #{e.message}"
+  rescue ActiveRecord::RecordNotFound
     render :qr_code_request, status: :not_found
   end
 end
