443c9fa1329d7b8f1043b009d4c6a9aaecf7087b
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-17 10:04:43 +0900

    Revert "Refactor handle_not_found test to use assert_select in ApplicationcontrollerTest"
    This reverts commit 0e7a954c1f54f71303e071edb04a119b7f72be13.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -17,10 +17,6 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test 'renders handle_not_found template' do
     get '/non_existent_page'
     assert_response :not_found
-
-    puts response.body
-
-    assert_select 'p', text: 'リソースが見つかりませんでした'
-    assert_select 'p', text: '最初からやり直してください'
+    assert_match '<p>リソースが見つかりませんでした</p>', response.body
   end
 end
