6db100e7cf47ea4bb4f225eeede5854e401e0eb8
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-18 20:46:07 +0900

    Replace create_session_path with sessions_path in SessionsControlleTest

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,7 +9,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   # '投稿者１'でのログインをテスト
   test 'should login first_poster user' do
     @user = users(:first_poster)
-    post create_session_path(@user)
+    post sessions_path
     assert_response :redirect
     assert_equal @user.id, session[:user_id].to_i
   end
