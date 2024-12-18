1ea9f4a2679de0e034f5e7e488eed483fef4b486
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-18 21:08:20 +0900

    Add handle_login_qr_code_path for setup to SessionControllerTest

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,6 +9,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   # '投稿者１'でのログインをテスト
   test 'should login first_poster user' do
     @user = users(:first_poster)
+    get handle_login_qr_code_path
     post sessions_path
     assert_response :redirect
     assert_equal @user.id, session[:user_id].to_i
