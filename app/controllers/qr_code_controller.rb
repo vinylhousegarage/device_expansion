3557a1b36a733a206e95fa8116a12c390d96e029
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-18 09:35:31 +0900

    Remove QrCodeRequestUrlGeneratorService from QrCodeController

--- app/controllers/qr_code_controller.rb
+++ app/controllers/qr_code_controller.rb
@@ -3,7 +3,6 @@ class QrCodeController < ApplicationController
 
   # QRコードを取得し@svgに格納
   def login_form
-    @url = QrCodeRequestUrlGeneratorService.generate_qr_code_request_url(@user)
     @svg = QrCodeGeneratorService.generate_qr_code(@user)
   end
 
