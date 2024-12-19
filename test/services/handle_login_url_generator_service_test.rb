94ceec980fae996dd554604ab1c0dd3f0e43cf09
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-19 13:49:03 +0900

    Revert "Fix HandleLogout renaming in HandleLogoutServiceTest"
    This reverts commit 8505c217ebf66a204e3538daf4df93b3ac50024d.

--- test/services/handle_login_url_generator_service_test.rb
+++ test/services/handle_login_url_generator_service_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HandleLoginUrlGeneratorServiceTest < ActiveSupport::TestCase
   test 'should generate correct handle_login URL' do
     user = users(:first_poster)
-    url = HandleLoginUrlGeneratorService.generate_handle_login_url(user)
+    url = QrCodeRequestUrlGeneratorService.generate_handle_login_url(user)
 
     expected_url = Rails.application.routes.url_helpers.handle_login_qr_code_url(
       user,
