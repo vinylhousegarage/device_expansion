44f8f4a4b243176b3179fe6a1e26d5654c05c7c3
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-19 13:50:07 +0900

    Revert "Change login_form route to use GET method in QrCodeLoginFormViewTest"
    This reverts commit b484f7f9f7215e84bfbcdda1fb65dafb7bfd452d.

--- test/views/qr_code/login_form_view_test.rb
+++ test/views/qr_code/login_form_view_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QrCodeLoginFormViewTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:first_poster)
-    get login_form_qr_code_path(@user)
+    post login_form_qr_code_path(@user)
     assert_response :success
   end
 
