ba3ed8a8142bc708e38d06c371a4ba9a95972693
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-19 15:11:06 +0900

    Refactor SessionsController: Rename create to login and destroy to logout for clarity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
   before_action :set_user, only: :create
 
   # 投稿者のログイン
-  def create
-    session[:user_id] = params[:id]
+  def login
+    session[:user_id] = @user.id
     redirect_to root_path
   end
 
   # 投稿者のログアウト
-  def destroy
+  def logout
     session[:user_id] = nil
   end
 end
