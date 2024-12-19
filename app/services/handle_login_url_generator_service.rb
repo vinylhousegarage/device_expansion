b12428353fbd61789f781f61ee58b4e03d961cec
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-19 13:48:58 +0900

    Revert "Fix handle_login URL generation: Pass user.id instead of user"
    This reverts commit a8df6474d7b0cad836f373ef1d5ebe9fce2522de.

--- app/services/handle_login_url_generator_service.rb
+++ app/services/handle_login_url_generator_service.rb
@@ -1,7 +1,7 @@
 class HandleLoginUrlGeneratorService
   def self.generate_handle_login_url(user)
     Rails.application.routes.url_helpers.handle_login_qr_code_url(
-      id: user.id
+      user,
       host: 'https://device-expansion.onrender.com'
     )
   end
