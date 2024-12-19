29001300016921750c51d65be4e05cc4a657d718
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-19 13:47:55 +0900

    Revert "Replace assert_response with success in handle_login_path test"
    This reverts commit bf264ba68b8fa52cb9d2bc5e2db2b04855cc8ce1.

--- test/views/qr_code/login_form_view_test.rb
+++ test/views/qr_code/login_form_view_test.rb
@@ -15,7 +15,7 @@ class QrCodeLoginFormViewTest < ActionDispatch::IntegrationTest
 
   test 'should navigate to handle_login path successfully' do
     get handle_login_qr_code_path(@user)
-    assert_response :success
+    assert_response :redirect
   end
 
   test 'should navigate to new_user path successfully' do
