7b23e60a619f0803006e79383a819b34666d3521
vinylhousegarage <vinylhousegarage@gmail.com>
2024-12-19 13:48:54 +0900

    Revert "Add missing ',' to fix URL generation in HandleLoginUrlGeneratorService"
    This reverts commit ec5648800b6f1e90a36ff294c4a194937e693cc9.

--- app/services/handle_login_url_generator_service.rb
+++ app/services/handle_login_url_generator_service.rb
@@ -1,7 +1,7 @@
 class HandleLoginUrlGeneratorService
   def self.generate_handle_login_url(user)
     Rails.application.routes.url_helpers.handle_login_qr_code_url(
-      id: user.id,
+      id: user.id
       host: 'https://device-expansion.onrender.com'
     )
   end
