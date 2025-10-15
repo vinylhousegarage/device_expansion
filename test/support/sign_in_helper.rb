8f510cb089af9a3ca5907308312211793879d2ec
vinylhousegarage <vinylhousegarage@gmail.com>
2025-10-15 13:18:11 +0900

    test(support): fix sign_in_as helper to safely handle Hash or User and avoid to_html errors

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -1,11 +1,47 @@
+# test/support/sign_in_helper.rb
 module SignInHelper
-  def sign_in_as(user, path = sessions_path, params: { id: user.id }, as: :html, method: :post)
+  def sign_in_as(user_or_id, path: nil, as: :html, method: :post, controller: (defined?(@controller) ? @controller : nil))
+    user_id = extract_user_id(user_or_id)
+
+    # Viewテスト(ActionView::TestCase)など request/response を持たない場合はセッション直書き
+    if !respond_to?(:post) && controller
+      controller.session[:user_id] = user_id
+      return
+    end
+
+    # Integration/Controllerテストは実リクエストで
+    path ||= (defined?(sessions_path) ? sessions_path : '/sessions')
+    params = { id: user_id }
+
     send(method, path, params:, as:)
     assert_response :success
   end
 
-  def admin_sign_in_as(user, path = admin_session_path(user.id), params: { id: user.id }, as: :html, method: :post)
+  def admin_sign_in_as(user_or_id, path: nil, as: :html, method: :post, controller: (defined?(@controller) ? @controller : nil))
+    user_id = extract_user_id(user_or_id)
+
+    # Viewテストならセッション直書き
+    if !respond_to?(:post) && controller
+      controller.session[:admin_user_id] = user_id
+      return
+    end
+
+    path ||= (defined?(admin_session_path) ? admin_session_path(user_id) : "/admin/sessions/#{user_id}")
+    params = { id: user_id }
+
     send(method, path, params:, as:)
     assert_response :redirect
+  end
+
+  private
+
+  def extract_user_id(user_or_id)
+    case user_or_id
+    when Integer then user_or_id
+    when String  then Integer(user_or_id) rescue user_or_id
+    when Hash    then user_or_id[:id] || user_or_id['id']
+    else
+      user_or_id.respond_to?(:id) ? user_or_id.id : user_or_id
+    end
   end
 end
