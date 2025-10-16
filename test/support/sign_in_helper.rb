217e31eb668bd06db95d881e2a8995683c259483
vinylhousegarage <vinylhousegarage@gmail.com>
2025-10-16 20:15:36 +0900

    test: accept optional :as in SignInHelper and pass it only for Integration tests

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -1,5 +1,5 @@
 module SignInHelper
-  def sign_in_as(user_or_id, path: nil, method: :post, controller: (defined?(@controller) ? @controller : nil))
+  def sign_in_as(user_or_id, path: nil, method: :post, as: nil, controller: (defined?(@controller) ? @controller : nil))
     user_id = extract_user_id(user_or_id)
 
     if view_test_context? || (!respond_to?(:post) && controller)
@@ -10,11 +10,16 @@ module SignInHelper
     path   ||= (defined?(sessions_path) ? sessions_path : '/sessions')
     params = { id: user_id }
 
-    send(method, path, params: params)
+    if integration_test_context? && !as.nil?
+      send(method, path, params: params, as: as)
+    else
+      send(method, path, params: params)
+    end
+
     assert_response :success
   end
 
-  def admin_sign_in_as(user_or_id, path: nil, method: :post, controller: (defined?(@controller) ? @controller : nil))
+  def admin_sign_in_as(user_or_id, path: nil, method: :post, as: nil, controller: (defined?(@controller) ? @controller : nil))
     user_id = extract_user_id(user_or_id)
 
     if view_test_context? || (!respond_to?(:post) && controller)
@@ -25,7 +30,12 @@ module SignInHelper
     path   ||= (defined?(admin_session_path) ? admin_session_path(user_id) : "/admin/sessions/#{user_id}")
     params = { id: user_id }
 
-    send(method, path, params: params)
+    if integration_test_context? && !as.nil?
+      send(method, path, params: params, as: as)
+    else
+      send(method, path, params: params)
+    end
+
     assert_response :redirect
   end
 
@@ -33,6 +43,10 @@ module SignInHelper
 
   def view_test_context?
     defined?(ActionView::TestCase) && is_a?(ActionView::TestCase)
+  end
+
+  def integration_test_context?
+    defined?(ActionDispatch::IntegrationTest) && is_a?(ActionDispatch::IntegrationTest)
   end
 
   def extract_user_id(user_or_id)
